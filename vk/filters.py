7fa60cfb698917e5cbf6c651f98e69e40b53f9d7
Daniil <dadyarri@gmail.com>
2021-04-13 21:15:50 +0300

    Add filter by button.

--- vk/filters.py
+++ vk/filters.py
@@ -1,8 +1,36 @@
-from vkwave.bots.core.dispatching.filters.base import BaseFilter
-from vkwave.bots.core.dispatching.filters.base import FilterResult
-from vkwave.bots import SimpleBotEvent
+from typing import AnyStr, Callable, Dict, Optional
+
+import ujson
+from vkwave.bots import BaseEvent
+from vkwave.bots.core import BaseFilter
+from vkwave.bots.core.dispatching import filters
 
 
 class OrderNewFilter(BaseFilter):
-    async def check(self, event: SimpleBotEvent) -> FilterResult:
-        return FilterResult(event.object.type == "market_order_new")
+    async def check(self, event: BaseEvent) -> filters.base.FilterResult:
+        return filters.base.FilterResult(event.object.type == "market_order_new")
+
+
+class ButtonFilter(BaseFilter):
+    def __init__(
+        self,
+        payload: str,
+        json_loader: Callable[[AnyStr], Dict] = ujson.loads,
+    ):
+        self.payload = {"button": payload}
+        self.json_loader = json_loader
+
+    async def check(self, event: BaseEvent) -> filters.base.FilterResult:
+        payload: Optional[str] = filters.builtin.get_payload(event)
+
+        if payload is None:
+            return filters.base.FilterResult(False)
+
+        current_payload = self.json_loader(payload)
+
+        return filters.base.FilterResult(
+            any(
+                self.payload.get(key, None) == val
+                for key, val in current_payload.items()
+            ),
+        )
