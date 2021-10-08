4cae18d291a120f480bf9e127a59a95916ea2cce
Daniil <dadyarri@gmail.com>
2021-10-08 23:05:48 +0300

    feat: add blueprints of api

---
+++ api/api/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/goods/")
+async def get_all_goods():
+    pass
+
+
+@app.get("/goods/{goods_id}")
+async def get_goods(goods_id: int):
+    pass
+
+
+@app.post("/goods/create")
+async def create_goods(name: str, wholesale_price: int, retail_price: int, leftover: int):
+    pass
+
+
+@app.post("/goods/rename/{goods_id}")
+async def rename_goods(new_name: str):
+    pass
+
+
+@app.post("/goods/increment/{goods_id}")
+async def increment_goods(goods_id: int):
+    pass
+
+
+@app.post("/goods/decrement/{goods_id}")
+async def decrement_goods(goods_id: int):
+    pass
+
+
+@app.post("/goods/price/wholesale/set/{goods_id}")
+async def set_wholesale_price(goods_id: int):
+    pass
+
+
+@app.post("/goods/price/retail/set/{goods_id}")
+async def set_retail_price(goods_id: int):
+    pass
