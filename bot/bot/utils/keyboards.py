e1de531e2209d31028aa7993f7429798954ed4da
Daniil <dadyarri@gmail.com>
2021-10-09 14:21:38 +0300

    feat(keyboards): add simple keyboard to list goods
    Signed-off-by: Daniil <dadyarri@gmail.com>

--- bot/bot/utils/keyboards.py
+++ bot/bot/utils/keyboards.py
@@ -16,3 +16,14 @@ def main_menu() -> str:
         ),
     )
     return kb.get_json()
+
+
+def list_goods(goods: list[dict]) -> str:
+    kb = Keyboard()
+    for good in goods:
+        kb.add(Text(good["name"], payload={"goods_id": good["id"]}))
+
+        if len(kb.buttons[-1]) == 2:
+            kb.row()
+
+    return kb.get_json()
