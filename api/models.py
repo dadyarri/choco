e5ff3be5596e2b932fe6a8a13014bb1e32cb6392
Daniil <dadyarri@gmail.com>
2021-10-14 23:11:27 +0300

    feat(models): add response models
    Signed-off-by: Daniil <dadyarri@gmail.com>

---
+++ api/models.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Good(BaseModel):
+    id: int
+    name: str
+    wholesale_price: int
+    retail_price: int
+    leftover: int
+    market_id: Optional[int]
+
+
+class BaseResponseModel(BaseModel):
+    response: str
+
+
+class BaseGoodResponse(BaseModel):
+    response: Good
+
+
+class GetAllGoodsResponseModel(BaseModel):
+    count: int
+    items: list[Good]
+
+
+class GetAllGoodsResponse(BaseModel):
+    response: GetAllGoodsResponseModel
