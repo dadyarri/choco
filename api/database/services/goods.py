3f6567d518dda8d002466619ccafd207d515c041
Daniil <dadyarri@gmail.com>
2021-10-22 02:57:31 +0300

    fix(api): fix returning value in increment/decrement
    Signed-off-by: Daniil <dadyarri@gmail.com>

--- api/database/services/goods.py
+++ api/database/services/goods.py
@@ -62,13 +62,13 @@ async def update_good_leftover(good_id: int, value: float):
 async def increment_good_leftover(good_id: int):
     async with in_transaction():
         good = await models.Good.get(id=good_id)
-        await update_good_leftover(good_id, good.leftover + 1)
+        return await update_good_leftover(good_id, good.leftover + 1)
 
 
 async def decrement_good_leftover(good_id: int):
     async with in_transaction():
         good = await models.Good.get(id=good_id)
-        await update_good_leftover(good_id, good.leftover - 1)
+        return await update_good_leftover(good_id, good.leftover - 1)
 
 
 async def change_good_wholesale_price(good_id: int, new_price: int):
