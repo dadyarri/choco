cd1e906517ede958a67cfe484b32af24e04257a7
Daniil <dadyarri@gmail.com>
2021-10-31 18:50:54 +0300

    feat: add filtering by callback data
    Signed-off-by: Daniil <dadyarri@gmail.com>

--- tg/utils/filters.py
+++ tg/utils/filters.py
@@ -1,3 +1,4 @@
+import json
 import os
 import typing
 
@@ -20,3 +21,19 @@ class IsAdmin(AbstractFilter):
 
     async def check(self, message: types.Message):
         return (message.from_user.id in self.admin_ids) == self.is_admin
+
+
+class CallbackFilter(AbstractFilter):
+    def __init__(self, data: dict):
+        self.callback_data = data
+
+    @classmethod
+    def validate(
+        cls, full_config: typing.Dict[str, typing.Any]
+    ) -> typing.Optional[typing.Dict[str, typing.Any]]:
+        pass
+
+    async def check(self, query: types.CallbackQuery) -> bool:
+        return all(
+            json.loads(query.data).get(k) == v for k, v in self.callback_data.items()
+        )
