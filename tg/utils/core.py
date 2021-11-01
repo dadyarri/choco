0e2027ce43428dad89534939635b870575ab12d4
Daniil <dadyarri@gmail.com>
2021-11-01 09:59:34 +0300

    feat: add some utils
    Signed-off-by: Daniil <dadyarri@gmail.com>

--- tg/utils/core.py
+++ tg/utils/core.py
@@ -1,7 +1,23 @@
 import os
+from typing import Union
 
 
 def get_tg_token() -> str:
     if os.getenv("ENV") == "DEV":
         return os.getenv("TG_TOKEN_DEV")
     return os.getenv("TG_TOKEN")
+
+
+def is_float(leftover: float) -> bool:
+    if leftover - int(leftover) == 0:
+        return False
+    return True
+
+
+def round_leftover(leftover: float) -> Union[float, int]:
+    if is_float(leftover):
+        res = round(leftover, 1)
+    else:
+        res = int(leftover)
+
+    return res
