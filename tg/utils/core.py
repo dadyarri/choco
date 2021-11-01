fadf7aa8025bb03689ec4aa39886d0cc4146fcca
Daniil <dadyarri@gmail.com>
2021-11-01 11:21:26 +0300

    feat: add utils to generate post message
    Signed-off-by: Daniil <dadyarri@gmail.com>

--- tg/utils/core.py
+++ tg/utils/core.py
@@ -1,6 +1,8 @@
 import os
 from typing import Union
 
+from utils.client import ChocoManagerClient
+
 
 def get_tg_token() -> str:
     if os.getenv("ENV") == "DEV":
@@ -21,3 +23,19 @@ def round_leftover(leftover: float) -> Union[float, int]:
         res = int(leftover)
 
     return res
+
+
+async def generate_post_message():
+    result = await get_all_goods()
+    return "\n".join(result)
+
+
+async def get_all_goods():
+    client = ChocoManagerClient()
+    resp = await client.get_all_goods()
+    result = []
+    for item in resp.response.items:
+        if item.leftover:
+            leftover = round_leftover(item.leftover)
+            result.append(f"{item.name} x{leftover} ({item.retail_price}₽)")
+    return result
