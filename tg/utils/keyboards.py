5136dc93a672be8c746bcb1a0db88a7bea2dee08
Daniil <dadyarri@gmail.com>
2021-11-01 08:16:56 +0300

    feat: add keyboard to list goods
    Signed-off-by: Daniil <dadyarri@gmail.com>

--- tg/utils/keyboards.py
+++ tg/utils/keyboards.py
@@ -2,22 +2,23 @@ import json
 
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+from utils.client import Good
+
 
 def main_menu_markup():
     kb = InlineKeyboardMarkup()
-    kb.add(
+    kb.insert(
         InlineKeyboardButton(
             text="Остатки",
-            callback_data=json.dumps({"block": "manage_leftovers", "action": "init"}),
+            callback_data=json.dumps({"block": "leftovers", "action": "init"}),
         ),
     )
-    kb.add(
+    kb.insert(
         InlineKeyboardButton(
             text="Диалоги",
             callback_data=json.dumps({"block": "manage_dialogs", "action": "init"}),
         )
     )
-    kb.row()
     kb.add(
         InlineKeyboardButton(
             text="Обновить пост",
@@ -26,3 +27,41 @@ def main_menu_markup():
     )
 
     return kb
+
+
+def list_goods(goods: list[Good]):
+    buttons = [[]]
+    for good in goods:
+        if len(buttons[-1]) == 2:
+            buttons.append([])
+
+        buttons[-1].append(
+            InlineKeyboardButton(
+                text=good.name,
+                callback_data=json.dumps(
+                    {
+                        "block": "leftovers",
+                        "action": "select_product",
+                        "value": good.id,
+                    }
+                ),
+            )
+        )
+
+    if buttons[-1]:
+        buttons.append([])
+
+    backward = InlineKeyboardButton(
+        text="<",
+        callback_data=json.dumps({"block": "leftovers", "action": "backward"}),
+    )
+    back = InlineKeyboardButton(
+        text="Назад", callback_data=json.dumps({"block": "main_menu"})
+    )
+    forward = InlineKeyboardButton(
+        text=">",
+        callback_data=json.dumps({"block": "leftovers", "action": "forward"}),
+    )
+    buttons[-1].extend([backward, back, forward])
+
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
