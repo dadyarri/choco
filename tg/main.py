13b621bea61c6e6d261359b34e4378324835b6ef
Daniil <dadyarri@gmail.com>
2021-11-12 21:11:47 +0300

    feat: add showing city of client
    Signed-off-by: Daniil <dadyarri@gmail.com>

--- tg/main.py
+++ tg/main.py
@@ -261,7 +261,8 @@ async def _dialog_menu(query: types.CallbackQuery):
     user = (await vk.users.get([str(chat.response.vk_id)]))[0]
     full_name = f"{user.first_name} {user.last_name}"
     await query.message.edit_text(
-        f"Диалог с {full_name}", reply_markup=dialog_menu(chat_id)
+        f"Диалог с {full_name}\nГород: {user.city or 'Не указно'}",
+        reply_markup=dialog_menu(chat_id),
     )
     await query.answer()
 
