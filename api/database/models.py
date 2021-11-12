a5c9d08e5ed7f12426d3af4ab91c905b5e5a44e6
Daniil <dadyarri@gmail.com>
2021-11-12 15:11:30 +0300

    feat(database): add model with list of chats

--- api/database/models.py
+++ api/database/models.py
@@ -8,3 +8,9 @@ class Good(Model):
     retail_price = fields.IntField()
     leftover = fields.FloatField()
     market_id = fields.IntField(null=True)
+
+
+class Chat(Model):
+    id = fields.IntField(pk=True)
+    vk_id = fields.IntField()
+    is_active = fields.BooleanField()
