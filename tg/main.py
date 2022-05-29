6523790f3bceb325896a2da51a589ea9d6c8aeff
Daniil <dadyarri@gmail.com>
2022-05-29 22:59:45 +0300

    fix(tg): fix filter name

--- tg/main.py
+++ tg/main.py
@@ -258,7 +258,7 @@ async def _dialogs_ask_message(query: types.CallbackQuery):
     await query.answer()
 
 
-@dp.message_handler(state=States.input_message, command="cancel")
+@dp.message_handler(state=States.input_message, commands=["cancel"])
 async def _cancel_send_message_to_vk(message: types.Message, state: FSMContext):
     await state.finish()
     await message.edit_text("Отправка отменена", reply_markup=main_menu_markup())
