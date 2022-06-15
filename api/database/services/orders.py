ebd2f67afff0d9482387ad9f53d1c5e8b4065196
Daniil <dadyarri@gmail.com>
2022-06-15 11:50:33 +0300

    feat(api): add utilities to work with orders in database

---
+++ api/database/services/orders.py
@@ -0,0 +1,16 @@
+from tortoise.transactions import in_transaction
+
+from database import models
+
+
+async def fetch_all_orders(page) -> list[models.Order]:
+    async with in_transaction():
+        if page == 0:
+            return await models.Order.all().order_by("id")
+        limit = 4
+        return (
+            await models.Order.all()
+            .order_by("vk_id")
+            .limit(limit)
+            .offset((page - 1) * limit)
+        )
