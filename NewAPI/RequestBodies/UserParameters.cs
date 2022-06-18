92e0c9d85cb209de99dcebb266acb59680a54279
Daniil <dadyarri@gmail.com>
2022-06-18 11:54:28 +0300

    feat(apinew): add request body for user

---
+++ NewAPI/RequestBodies/UserParameters.cs
@@ -0,0 +1,7 @@
+namespace NewAPI.RequestBodies;
+
+public class UserParameters
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
