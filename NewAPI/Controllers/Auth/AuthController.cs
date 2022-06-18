a3b12c2b994d6029721f11bab4b8a88aa3fb449c
Daniil <dadyarri@gmail.com>
2022-06-18 13:27:20 +0300

    refactor(apinew): little refactor

--- NewAPI/Controllers/Auth/AuthController.cs
+++ NewAPI/Controllers/Auth/AuthController.cs
@@ -61,10 +61,8 @@ public class AuthController : ControllerBase
 
     private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
     {
-        using (var hmac = new HMACSHA512())
-        {
-            passwordSalt = hmac.Key;
-            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-        }
+        using var hmac = new HMACSHA512();
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
     }
 }
