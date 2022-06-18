ba01316a5a3360a97b7b92bcbc1448543d379ea9
Daniil <dadyarri@gmail.com>
2022-06-18 13:59:50 +0300

    feat(apinew): add creating jwt and login endpoint

--- NewAPI/Controllers/Auth/AuthController.cs
+++ NewAPI/Controllers/Auth/AuthController.cs
@@ -1,8 +1,13 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using NewAPI.Data;
 using NewAPI.Models;
 using NewAPI.RequestBodies;
+using NewAPI.Responses;
 using Npgsql;
 
 namespace NewAPI.Controllers.Auth;
@@ -14,22 +19,24 @@ namespace NewAPI.Controllers.Auth;
 [Route("api/v2/[controller]")]
 public class AuthController : ControllerBase
 {
+    private readonly IConfiguration _configuration;
     private readonly ApplicationContext _db;
 
     /// <summary>
     /// Конструктор контроллера
     /// </summary>
     /// <param name="context">Автоматически добавляемый контекст базы данных</param>
-    public AuthController(ApplicationContext context)
+    public AuthController(ApplicationContext context, IConfiguration configuration)
     {
         _db = context;
+        _configuration = configuration;
     }
 
     /// <summary>
     /// Регистрация пользователя
     /// </summary>
     /// <param name="body">Тело запроса с именем пользователя и паролем</param>
-    [HttpPost]
+    [HttpPost("Register")]
     public async Task<ActionResult<User>> Register(UserParameters body)
     {
         CreatePasswordHash(body.Password, out byte[] passwordHash, out byte[] passwordSalt);
@@ -57,6 +64,57 @@ public class AuthController : ControllerBase
         }
 
         return Created("/api/v2/Auth", user);
+    }
+
+
+    [HttpPost("Login")]
+    public async Task<ActionResult<string>> Login(UserParameters body)
+    {
+        var user = await _db.Users.SingleOrDefaultAsync(u => u.Username.Equals(body.Username));
+
+        if (user is null)
+        {
+            return BadRequest(new Error
+            {
+                Code = 400,
+                Message = "User was not found",
+                Data = body.Username
+            });
+        }
+
+        if (!VerifyPasswordHash(body.Password, user.PasswordHash, user.PasswordSalt))
+        {
+            return BadRequest(new Error
+            {
+                Code = 400,
+                Message = "Wrong password"
+            });
+        }
+
+        return Ok(GenerateToken(user));
+    }
+
+    private string GenerateToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Name, user.Username)
+        };
+
+        var key = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("Security:Token").Value));
+
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddDays(1),
+            signingCredentials: credentials
+        );
+
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return jwt;
     }
 
     private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
