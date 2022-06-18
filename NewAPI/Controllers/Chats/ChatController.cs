b907f302cafc01cd03694f65fd1d0aaf469dee5c
Daniil <dadyarri@gmail.com>
2022-06-18 09:59:08 +0300

    feat(apinew): add chat controller

---
+++ NewAPI/Controllers/Chats/ChatController.cs
@@ -0,0 +1,124 @@
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NewAPI.Data;
+using NewAPI.Models;
+
+namespace NewAPI.Controllers.Chats;
+
+/// <summary>
+/// Работа с отдельными элементами коллекции чатов
+/// </summary>
+[ApiController]
+[Route("/api/v2/[controller]")]
+public class ChatController : ControllerBase
+{
+    private readonly ApplicationContext _db;
+
+    /// <summary>
+    /// Конструктор контроллера
+    /// </summary>
+    /// <param name="context">Автоматически добавляемый контекст базы данных</param>
+    public ChatController(ApplicationContext context)
+    {
+        _db = context;
+    }
+
+    /// <summary>
+    /// Получение чата по идентификатору
+    /// </summary>
+    /// <param name="id">Идентификатор чата</param>
+    /// <response code="200">Чат найден</response>
+    /// <response code="404">Чат не найден</response>
+    [HttpGet("{id:int}")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Chat>> GetChatById(int id)
+    {
+        var chat = await _db.Chats.FindAsync(id);
+
+        if (chat is null)
+        {
+            return NotFound(id);
+        }
+
+        return chat;
+    }
+
+    /// <summary>
+    /// Получение чата по идентификатору ВК
+    /// </summary>
+    /// <param name="vkId">Идентификатор чата в ВК</param>
+    /// <response code="200">Чат найден</response>
+    /// <response code="404">Чат не найден</response>
+    [HttpGet("vk/{vkId:int}")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Chat>> GetChatByVkId(int vkId)
+    {
+        try
+        {
+            return await _db.Chats.SingleAsync(p => p.VkId == vkId);
+        }
+        catch (InvalidOperationException)
+        {
+            return NotFound(vkId);
+        }
+    }
+
+    /// <summary>
+    /// Обновление чата
+    /// </summary>
+    /// <param name="id">Идентификатор чата</param>
+    /// <param name="chat">Запрос в формате JSON PATCH, обновляющий ресурс</param>
+    /// <response code="400">Тело запроса не передано или не валидно</response>
+    /// <response code="404">Чат не найден</response>
+    /// <response code="200">Чат изменён</response>
+    [HttpPatch("{id:int}")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Product>> UpdateChat(int id, [FromBody] JsonPatchDocument<Chat>? chat)
+    {
+        if (chat == null) return BadRequest(chat);
+
+        var entity = await _db.Chats.FindAsync(id);
+
+        if (entity == null)
+        {
+            return NotFound(id);
+        }
+
+        chat.ApplyTo(entity, ModelState);
+        await _db.SaveChangesAsync();
+        return Ok(entity);
+    }
+
+    /// <summary>
+    /// Удаление чата
+    /// </summary>
+    /// <param name="id">Идентификатор чата</param>
+    /// <response code="404">Чат не найден</response>
+    /// <response code="204">Чат удалён</response>
+    [HttpDelete("{id:int}")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> DeleteChat(int id)
+    {
+        var entity = await _db.Chats.FindAsync(id);
+
+        if (entity is null)
+        {
+            return NotFound(id);
+        }
+
+        _db.Chats.Remove(entity);
+        await _db.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
