7e79af24329d65584f480e652c60a8970d8e9fa6
Daniil <dadyarri@gmail.com>
2022-06-18 12:13:46 +0300

    docs(apinew): add docs

--- NewAPI/Data/ApplicationContext.cs
+++ NewAPI/Data/ApplicationContext.cs
@@ -3,18 +3,56 @@ using NewAPI.Models;
 
 namespace NewAPI.Data;
 
+/// <summary>
+/// Контекст базы данных
+/// </summary>
 public class ApplicationContext : DbContext
 {
+    /// <summary>
+    /// Конструктор контекста
+    /// </summary>
+    /// <param name="options">Параметры контекста</param>
     public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
     {
     }
 
+    /// <summary>
+    /// Коллекция чатов
+    /// </summary>
     public DbSet<Chat> Chats => Set<Chat>();
+
+    /// <summary>
+    /// Коллекция заказов
+    /// </summary>
     public DbSet<Order> Orders => Set<Order>();
+
+    /// <summary>
+    /// Коллекция городов, доступных для совершения заказа
+    /// </summary>
     public DbSet<OrderCity> OrderCities => Set<OrderCity>();
+
+    /// <summary>
+    /// Коллекция заказанных товаров
+    /// </summary>
     public DbSet<OrderedProduct> OrderedProducts => Set<OrderedProduct>();
+
+    /// <summary>
+    /// Коллекция источников заказа
+    /// </summary>
     public DbSet<OrderSource> OrderSources => Set<OrderSource>();
+
+    /// <summary>
+    /// Коллекция состояний заказа
+    /// </summary>
     public DbSet<OrderState> OrderStates => Set<OrderState>();
+
+    /// <summary>
+    /// Коллекция товаров
+    /// </summary>
     public DbSet<Product> Products => Set<Product>();
+
+    /// <summary>
+    /// Коллекция пользователей
+    /// </summary>
     public DbSet<User> Users => Set<User>();
 }
