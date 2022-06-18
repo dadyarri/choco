500ac4cf4b07b362b9dd7f428d8c2f62f7b190e2
Daniil <dadyarri@gmail.com>
2022-06-18 13:27:39 +0300

    feat(apinew): add verifying password

--- NewAPI/Controllers/Auth/AuthController.cs
+++ NewAPI/Controllers/Auth/AuthController.cs
@@ -65,4 +65,11 @@ public class AuthController : ControllerBase
         passwordSalt = hmac.Key;
         passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
     }
+
+    private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+        using var hmac = new HMACSHA512(passwordSalt);
+        var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        return computedHash.SequenceEqual(passwordHash);
+    }
 }
