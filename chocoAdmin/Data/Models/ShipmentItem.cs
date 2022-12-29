0d50964889195d948f2ee9a08d083f2f9e25bd77
Daniil <dadyarri@gmail.com>
2022-12-30 00:45:17 +0300

    fix: fix circular reference

--- chocoAdmin/Data/Models/ShipmentItem.cs
+++ chocoAdmin/Data/Models/ShipmentItem.cs
@@ -2,7 +2,6 @@ namespace choco.Data.Models;
 
 public class ShipmentItem: BaseModel
 {
-    public Shipment Shipment { get; set; }
     public Product Product { get; set; }
     public double Amount { get; set; }
     public ShipmentItemStatus Status { get; set; }
