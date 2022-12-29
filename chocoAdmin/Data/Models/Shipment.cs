7f7563848b58485783a95bbbcd303d14e5e73c30
Daniil <dadyarri@gmail.com>
2022-12-30 01:23:33 +0300

    feat: add status to shipment

--- chocoAdmin/Data/Models/Shipment.cs
+++ chocoAdmin/Data/Models/Shipment.cs
@@ -3,5 +3,6 @@ namespace choco.Data.Models;
 public class Shipment: BaseModel
 {
     public DateOnly Date { get; set; }
+    public ShipmentStatus Status { get; set; }
     public List<ShipmentItem> ShipmentItems { get; set; }
 }
