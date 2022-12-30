b7764fd4846e9bb579f59468ca6f6231a489f0e5
Daniil <dadyarri@gmail.com>
2022-12-30 11:09:23 +0300

    feat: add label for date

--- chocoAdmin/ClientApp/src/components/CreateShipment.js
+++ chocoAdmin/ClientApp/src/components/CreateShipment.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import {Label} from "reactstrap";
 
 export class CreateShipment extends Component {
     static displayName = CreateShipment.name;
@@ -15,7 +16,8 @@ export class CreateShipment extends Component {
                 <Formik initialValues={{date: ''}} onSubmit={values => console.log(values)}>
                     <Form style={{maxWidth: 400, margin: 20}}>
                         <div className={"form-group m-3"}>
-                            <Field type={"date"} name={"date"} className={"form-control"}/>
+                            <Label for={"dateInput"}>Дата поставки</Label>
+                            <Field type={"date"} name={"date"} id={"dateInput"} className={"form-control"}/>
                         </div>
                         <button type="submit" className={"btn btn-success"}>
                             Сохранить
