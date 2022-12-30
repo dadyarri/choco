22a52131f8dd6968e8255cdb20151db81cd9d6e0
Daniil <dadyarri@gmail.com>
2022-12-30 11:43:14 +0300

    feat: add dynamic adding new products in form

--- chocoAdmin/ClientApp/src/components/CreateShipment.js
+++ chocoAdmin/ClientApp/src/components/CreateShipment.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import {Formik, Form, Field, ErrorMessage, FieldArray} from 'formik';
 import {Label} from "reactstrap";
+import {v4 as uuid} from 'uuid';
 
 export class CreateShipment extends Component {
     static displayName = CreateShipment.name;
@@ -13,16 +14,57 @@ export class CreateShipment extends Component {
         return (
             <div>
                 <h1>Создание поставки</h1>
-                <Formik initialValues={{date: ''}} onSubmit={values => console.log(values)}>
-                    <Form style={{maxWidth: 400, margin: 20}}>
-                        <div className={"form-group m-3"}>
-                            <Label for={"dateInput"}>Дата поставки</Label>
-                            <Field type={"date"} name={"date"} id={"dateInput"} className={"form-control"}/>
-                        </div>
-                        <button type="submit" className={"btn btn-success"}>
-                            Сохранить
-                        </button>
-                    </Form>
+                <Formik initialValues={{date: '', products: []}} onSubmit={values => console.log(values)}>
+                    {({values}) => (
+                        <Form style={{maxWidth: 400, margin: 20}}>
+                            <div className={"form-group m-3"}>
+                                <Label for={"dateInput"}>Дата поставки</Label>
+                                <Field type={"date"} name={"date"} id={"dateInput"} className={"form-control"}/>
+                            </div>
+
+                            <FieldArray
+                                name={"products"}
+                                render={arrayHelpers => (
+                                    <div className={"m-3"}>
+                                        {values.products.map((product, index) => (
+                                            <div className={"d-flex align-items-center m-3"} key={uuid()}>
+                                                <div key={uuid()}>
+                                                    <div key={uuid()} className={"form-group m-3"}>
+                                                        <Label for={`productsName[${index}]Input`}>Название продукта</Label>
+                                                        <Field name={`products[${index}].name`}
+                                                               id={`productsName[${index}]Input`}
+                                                               className={"form-control"}/>
+                                                    </div>
+                                                    <div key={uuid()} className={"form-group m-3"}>
+                                                        <Label for={`productsAmount[${index}]Input`}>Количество</Label>
+                                                        <Field name={`products[${index}].amount`}
+                                                               id={`productsAmount[${index}]Input`}
+                                                               className={"form-control"}
+                                                               type={"number"}
+                                                        />
+                                                    </div>
+                                                </div>
+                                                <button type={"button"} onClick={() => arrayHelpers.remove(index)}
+                                                        className={"btn btn-danger"} style={{maxHeight: 40}}
+                                                        key={uuid()}
+                                                >-
+                                                </button>
+                                            </div>
+                                        ))}
+                                        <button type={"button"}
+                                                onClick={() => arrayHelpers.push({name: '', amount: ''})}
+                                                className={"btn btn-primary"}
+                                        >+ Добавить товар
+                                        </button>
+                                    </div>
+                                )}
+                            />
+
+                            <button type="submit" className={"btn btn-success"}>
+                                Сохранить
+                            </button>
+                        </Form>
+                    )}
                 </Formik>
             </div>
         );
