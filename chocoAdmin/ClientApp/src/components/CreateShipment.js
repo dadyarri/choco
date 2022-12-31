95b0a4ef748acf1fb6921fea628ee24d9ee26b7c
Daniil <dadyarri@gmail.com>
2023-01-01 01:50:31 +0300

    feat: add form validation

--- chocoAdmin/ClientApp/src/components/CreateShipment.js
+++ chocoAdmin/ClientApp/src/components/CreateShipment.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
-import {Field, FieldArray, Form, Formik} from 'formik';
+import {Field, FieldArray, Form, Formik, ErrorMessage} from 'formik';
 import {Label} from "reactstrap";
 import axios from "axios";
+import * as Yup from 'yup';
 
 export class CreateShipment extends Component {
     static displayName = CreateShipment.name;
+    validationSchema = Yup.object().shape({
+        date: Yup.date().required("Дата обязательное поле"),
+        products: Yup.array().of(
+            Yup.object().shape({
+                name: Yup.string().uuid("Неверный формат идентификатора!").required("Выбрать продукт обязательно!"),
+                amount: Yup.number().required("Количество товара обязательно!").positive("Количество товара не может быть меньше единицы")
+            })
+        )
+    })
 
     constructor(props) {
         super(props);
@@ -15,13 +25,18 @@ export class CreateShipment extends Component {
         return (
             <div>
                 <h1>Создание поставки</h1>
-                <Formik initialValues={{date: '', products: []}} onSubmit={values => console.log(values)}>
-                    {({values}) => (
+                <Formik
+                    initialValues={{date: '', products: []}}
+                    onSubmit={values => console.log(values)}
+                    validationSchema={this.validationSchema}
+                >
+                    {({values, errors, touched}) => (
                         <Form style={{maxWidth: 400, margin: 20}} key={"productForm"}>
                             <div className={"form-group m-3"} key={"shipmentDateInputGroup"}>
                                 <Label for={"dateInput"} key={"shipmentDateLabel"}>Дата поставки</Label>
                                 <Field type={"date"} name={"date"} id={"dateInput"} className={"form-control"}
                                        key={"shipmentDateInput"}/>
+                                <ErrorMessage name="date" />
                             </div>
                             <div className={"form-group m-3"}>
                                 <Label for={"arrayOfProducts"} key={"arrayOfProductsLabel"}>Товары, входящие в
@@ -55,6 +70,8 @@ export class CreateShipment extends Component {
                                                                             key={product.id}>{product.name}</option>
                                                                 ))}
                                                             </Field>
+                                                            <ErrorMessage name={`products[${index}].name`}/>
+                                                            
                                                         </div>
                                                         <div className={"form-group m-3"}
                                                              key={`arrayOfProductsAmountInputGroup_${index}`}>
@@ -66,6 +83,7 @@ export class CreateShipment extends Component {
                                                                    type={"number"}
                                                                    key={`arrayOfProductsAmountInput_${index}`}
                                                             />
+                                                            <ErrorMessage name={`products[${index}].amount`}/>
                                                         </div>
                                                     </div>
                                                     <button type={"button"} onClick={() => arrayHelpers.remove(index)}
