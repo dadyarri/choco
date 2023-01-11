0c32fe69a4b680615f2085d64ae37198fef0233d
Daniil <dadyarri@gmail.com>
2023-01-11 14:07:03 +0300

    feat: add creating order backend work

--- chocoAdmin/Controllers/OrdersController.cs
+++ chocoAdmin/Controllers/OrdersController.cs
@@ -1,4 +1,6 @@
 using choco.Data;
+using choco.Data.Models;
+using choco.RequestBodies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -25,5 +27,40 @@ public class OrdersController : ControllerBase
             .Include(o => o.Address)
             .ThenInclude(a => a.City)
             .ToListAsync());
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateOrder([FromBody] CreateOrderRequestBody body)
+    {
+        var order = new Order
+        {
+            Date = DateOnly.ParseExact(body.Date, "yyyy-mm-dd"),
+            Address = new OrderAddress
+            {
+                Building = body.Address.Building,
+                City = await _db.OrderCities.FindAsync(body.Address.City),
+                Street = body.Address.Street
+            },
+            OrderItems = await findOrderItems(body.OrderItems),
+            Status = await _db.OrderStatuses.FindAsync(body.Status)
+        };
+        await _db.Orders.AddAsync(order);
+        await _db.SaveChangesAsync();
+        return Created("/api/Orders", order);
+    }
+
+    private async Task<List<OrderItem>> findOrderItems(List<CreateOrderItemsRequestBody> source)
+    {
+        var items = new List<OrderItem>();
+        foreach (var sourceItem in source)
+        {
+            items.Add(new OrderItem
+            {
+                Amount = sourceItem.Amount,
+                Product = await _db.Products.FindAsync(sourceItem.Id)
+            });
+        }
+
+        return items;
     }
 }
