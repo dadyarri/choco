96913e9f2b02460489ee7c2f05e16766c4bbd40b
Daniil <dadyarri@gmail.com>
2023-01-12 02:53:47 +0300

    feat: add chart with sells by city

---
+++ chocoAdmin/ClientApp/src/components/Charts/StatsByCityChart.js
@@ -0,0 +1,85 @@
+import React, { PureComponent } from 'react';
+import {PieChart, Pie, Sector, ResponsiveContainer, Cell} from 'recharts';
+
+const renderActiveShape = (props) => {
+    const RADIAN = Math.PI / 180;
+    const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius + 10) * cos;
+    const sy = cy + (outerRadius + 10) * sin;
+    const mx = cx + (outerRadius + 30) * cos;
+    const my = cy + (outerRadius + 30) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? 'start' : 'end';
+
+    return (
+        <g>
+            <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+                {payload.name}
+            </text>
+            <Sector
+                cx={cx}
+                cy={cy}
+                innerRadius={innerRadius}
+                outerRadius={outerRadius}
+                startAngle={startAngle}
+                endAngle={endAngle}
+                fill={fill}
+            />
+            <Sector
+                cx={cx}
+                cy={cy}
+                startAngle={startAngle}
+                endAngle={endAngle}
+                innerRadius={outerRadius + 6}
+                outerRadius={outerRadius + 10}
+                fill={fill}
+            />
+            <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
+            <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+            <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">
+                {`${(percent * 100).toFixed(2)}%`}
+            </text>
+        </g>
+    );
+};
+
+export default class StatsByCityChart extends PureComponent {
+    state = {
+        activeIndex: 0,
+    };
+
+    onPieEnter = (_, index) => {
+        this.setState({
+            activeIndex: index,
+        });
+    };
+
+    COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+    render() {
+        const { data } = this.props;
+        return (
+            <PieChart width={350} height={300}>
+                <Pie
+                    activeIndex={this.state.activeIndex}
+                    activeShape={renderActiveShape}
+                    data={data}
+                    cx="50%"
+                    cy="50%"
+                    innerRadius={60}
+                    outerRadius={80}
+                    fill="#8884d8"
+                    dataKey="value"
+                    onMouseEnter={this.onPieEnter}
+                >
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={this.COLORS[index % this.COLORS.length]} />
+                    ))}
+                </Pie>
+            </PieChart>
+        );
+    }
+}
