947e764d1033fea52412119eb78dbcd050c7e35b
Daniil <dadyarri@gmail.com>
2023-01-11 14:06:43 +0300

    feat: add models to represent create order request

---
+++ chocoAdmin/RequestBodies/CreateOrderRequestBody.cs
@@ -0,0 +1,22 @@
+namespace choco.RequestBodies;
+
+public class CreateOrderRequestBody
+{
+    public string Date { get; set; }
+    public Guid Status { get; set; }
+    public List<CreateOrderItemsRequestBody> OrderItems { get; set; }
+    public CreateOrderAddressRequestBody Address {get; set;}
+    }
+
+public class CreateOrderAddressRequestBody
+{
+    public Guid City { get; set; }
+    public string Street { get; set; }
+    public string Building { get; set; }
+}
+
+public class CreateOrderItemsRequestBody
+{
+    public Guid Id { get; set; }
+    public int Amount { get; set; }
+}
