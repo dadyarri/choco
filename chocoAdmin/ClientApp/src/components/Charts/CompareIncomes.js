6afceda2ee16741ff82dc8f6d27e24f282b4de0a
Daniil <dadyarri@gmail.com>
2023-01-12 21:39:59 +0300

    refactor: rename component

--- chocoAdmin/ClientApp/src/components/Charts/CompareIncomes.js
+++ chocoAdmin/ClientApp/src/components/Charts/CompareIncomes.js
@@ -1,7 +1,7 @@
 import {Badge} from "reactstrap";
 import {BsArrowDownRight, BsArrowUpRight} from "react-icons/bs";
 
-export const TotalIncomes = ({data}) => {
+export const CompareIncomes = ({data}) => {
     return <div className={"d-flex"} style={{width: "20rem"}}>
         <b className={"flex-grow-1"} style={{fontSize: "30px"}}>{data[0].total} &#8381;</b>&nbsp;
         <span>{data[0].total >= data[1].total ? <Badge
