6dce3f646aeda6a553fa04df9224b708f382d2bc
Daniil <dadyarri@gmail.com>
2023-01-12 13:30:18 +0300

    style: add some styles to total incomes widget

--- chocoAdmin/ClientApp/src/components/Charts/TotalIncomes.js
+++ chocoAdmin/ClientApp/src/components/Charts/TotalIncomes.js
@@ -2,8 +2,8 @@ import {Badge} from "reactstrap";
 import {BsArrowDownRight, BsArrowUpRight} from "react-icons/bs";
 
 export const TotalIncomes = ({data}) => {
-    return <div className={"d-flex"}>
-        <b style={{fontSize: "30px"}}>{data[0].total} &#8381;</b>&nbsp;
+    return <div className={"d-flex"} style={{width: "20rem"}}>
+        <b className={"flex-grow-1"} style={{fontSize: "30px"}}>{data[0].total} &#8381;</b>&nbsp;
         <span>{data[0].total >= data[1].total ? <Badge
             color="success"
             style={{fontSize: "20px"}}
