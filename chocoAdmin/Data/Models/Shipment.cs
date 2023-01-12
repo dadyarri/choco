7c65670130454f2f9787372bfbf2be701fa05bd1
Daniil <dadyarri@gmail.com>
2023-01-12 20:35:20 +0300

    db: add deleted column to shipment

--- chocoAdmin/Data/Models/Shipment.cs
+++ chocoAdmin/Data/Models/Shipment.cs
@@ -5,4 +5,5 @@ public class Shipment: BaseModel
     public DateOnly Date { get; set; }
     public ShipmentStatus Status { get; set; }
     public List<ShipmentItem> ShipmentItems { get; set; }
+    public bool Deleted { get; set; }
 }
