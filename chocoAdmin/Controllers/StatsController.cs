c53def454d46af5bd928693dbf7583fc58337f57
Daniil <dadyarri@gmail.com>
2023-01-12 13:05:51 +0300

    feat: add fetching stats about incomes

--- chocoAdmin/Controllers/StatsController.cs
+++ chocoAdmin/Controllers/StatsController.cs
@@ -1,4 +1,5 @@
 using choco.Data;
+using choco.Responses;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -24,5 +25,47 @@ public class StatsController : ControllerBase
             .Select(g => new { name = g.Key.Name, value = g.Count() })
             .ToListAsync();
         return Ok(data);
+    }
+
+    [HttpGet("CompareIncome")]
+    public async Task<ActionResult> GetIncomeInLastTwoMonths()
+    {
+        var currentMonthDate = DateTime.Now;
+        var previousMonthDate = DateTime.Now.AddMonths(-1);
+
+        var currentMonthData = await _db.Orders
+            .Include(o => o.OrderItems)
+            .ThenInclude(oi => oi.Product)
+            .Where(o => o.Date.Month == currentMonthDate.Month &&
+                        o.Date.Year == currentMonthDate.Year).ToListAsync();
+
+        var currentMonthIncome =
+            currentMonthData.Sum(order => order.OrderItems.Sum(oi => oi.Product.RetailPrice * oi.Amount));
+
+        var previousMonthData = await _db.Orders
+            .Include(o => o.OrderItems)
+            .ThenInclude(oi => oi.Product)
+            .Where(o => o.Date.Month == previousMonthDate.Month &&
+                        o.Date.Year == previousMonthDate.Year).ToListAsync();
+
+        var previousMonthIncome =
+            previousMonthData.Sum(order => order.OrderItems.Sum(oi => oi.Product.RetailPrice * oi.Amount));
+
+        return Ok(new IncomesResponse
+        {
+            IncomeInfos = new List<IncomeInfo>
+            {
+                new()
+                {
+                    DateInfo = $"{currentMonthDate.Month}/{currentMonthDate.Year}",
+                    Total = currentMonthIncome
+                },
+                new()
+                {
+                    DateInfo = $"{previousMonthDate.Month}/{previousMonthDate.Year}",
+                    Total = previousMonthIncome
+                }
+            }
+        });
     }
 }
