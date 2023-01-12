1b29a482a53e2482cab28a65192cb6327e252e11
Daniil <dadyarri@gmail.com>
2023-01-12 15:49:26 +0300

    feat: add endpoint to fetch shipment statuses

---
+++ chocoAdmin/Controllers/ShipmentStatusesController.cs
@@ -0,0 +1,24 @@
+using choco.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace choco.Controllers;
+
+[Controller]
+[Route("[controller]")]
+public class ShipmentStatusesController : ControllerBase
+{
+    private readonly AppDbContext _db;
+
+    public ShipmentStatusesController(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetAllShipmentStatuses()
+    {
+        return Ok(await _db.ShipmentStatuses
+            .ToListAsync());
+    }
+}
