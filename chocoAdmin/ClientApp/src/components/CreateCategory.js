9a4adc0d3a429547f657920a84e657c8fd548610
Daniil <dadyarri@gmail.com>
2023-01-14 00:06:31 +0300

    feat: add validation

--- chocoAdmin/ClientApp/src/components/CreateCategory.js
+++ chocoAdmin/ClientApp/src/components/CreateCategory.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {ErrorMessage, Field, Form, Formik} from 'formik';
-import {Label} from "reactstrap";
+import {FormFeedback, Input, Label} from "reactstrap";
 import axios from "axios";
 import * as Yup from 'yup';
 
@@ -29,8 +29,11 @@ export class CreateCategory extends Component {
                         <Form style={{maxWidth: 400, margin: 20}} key={"productForm"}>
                             <div className={"form-group m-3"}>
                                 <Label for={"nameInput"}>Название категории</Label>
-                                <Field name={"name"} id={"nameInput"} className={"form-control"}/>
-                                <ErrorMessage name="name"/>
+                                <Field name={"name"} id={"nameInput"} className={"form-control"} as={Input}
+                                       valid={!errors.name && touched.name}
+                                       invalid={errors.name && touched.name}
+                                />
+                                {errors.name && touched.name ? <FormFeedback>{errors.name}</FormFeedback> : null}
                             </div>
                             <button type="submit" className={"btn btn-success"}>
                                 Сохранить
