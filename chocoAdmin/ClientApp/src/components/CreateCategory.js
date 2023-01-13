c1f80b539dadd6c488a6004636ba13fad8cc1a41
Daniil <dadyarri@gmail.com>
2023-01-14 00:13:37 +0300

    refactor: rewrite as FC

--- chocoAdmin/ClientApp/src/components/CreateCategory.js
+++ chocoAdmin/ClientApp/src/components/CreateCategory.js
@@ -4,46 +4,39 @@ import {FormFeedback, Input, Label} from "reactstrap";
 import axios from "axios";
 import * as Yup from 'yup';
 
-export class CreateCategory extends Component {
-    static displayName = CreateCategory.name;
-    validationSchema = Yup.object().shape({
+export const CreateCategory = () => {
+    const validationSchema = Yup.object().shape({
         name: Yup.string().required("Поле обязательно!")
     })
 
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Создание категории</h1>
-                <Formik
-                    initialValues={{
-                        name: '',
-                    }}
-                    onSubmit={async values => {
-                        await axios.post("/api/productCategories", values);
-                    }}
-                    validationSchema={this.validationSchema}
-                >
-                    {({values, errors, touched}) => (
-                        <Form style={{maxWidth: 400, margin: 20}} key={"productForm"}>
-                            <div className={"form-group m-3"}>
-                                <Label for={"nameInput"}>Название категории</Label>
-                                <Field name={"name"} id={"nameInput"} className={"form-control"} as={Input}
-                                       valid={!errors.name && touched.name}
-                                       invalid={errors.name && touched.name}
-                                />
-                                {errors.name && touched.name ? <FormFeedback>{errors.name}</FormFeedback> : null}
-                            </div>
-                            <button type="submit" className={"btn btn-success"}>
-                                Сохранить
-                            </button>
-                        </Form>
-                    )}
-                </Formik>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <h1>Создание категории</h1>
+            <Formik
+                initialValues={{
+                    name: '',
+                }}
+                onSubmit={async values => {
+                    await axios.post("/api/productCategories", values);
+                }}
+                validationSchema={validationSchema}
+            >
+                {({values, errors, touched}) => (
+                    <Form style={{maxWidth: 400, margin: 20}} key={"productForm"}>
+                        <div className={"form-group m-3"}>
+                            <Label for={"nameInput"}>Название категории</Label>
+                            <Field name={"name"} id={"nameInput"} className={"form-control"} as={Input}
+                                   valid={!errors.name && touched.name}
+                                   invalid={errors.name && touched.name}
+                            />
+                            {errors.name && touched.name ? <FormFeedback>{errors.name}</FormFeedback> : null}
+                        </div>
+                        <button type="submit" className={"btn btn-success"}>
+                            Сохранить
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
 }
