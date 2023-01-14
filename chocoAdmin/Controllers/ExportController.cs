e7df7dd2aa42da9082523590249028a89e009766
Daniil <dadyarri@gmail.com>
2023-01-14 16:25:21 +0300

    feat: add generation of image with leftovers

--- chocoAdmin/Controllers/ExportController.cs
+++ chocoAdmin/Controllers/ExportController.cs
@@ -1,6 +1,7 @@
 using choco.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using SkiaSharp;
 
 namespace choco.Controllers;
 
@@ -18,6 +19,48 @@ public class ExportController : ControllerBase
     public async Task<ActionResult> ExportImage()
     {
         var products = await _db.Products.Where(p => p.Leftover > 0 && !p.Deleted).ToListAsync();
-        return Ok();
+        SKData data;
+
+        const float xPadding = 50.0f;
+        const float yPadding = 50.0f;
+
+        var imageInfo = new SKImageInfo(600, 600);
+        using (var surface = SKSurface.Create(imageInfo))
+        {
+            var canvas = surface.Canvas;
+            canvas.Clear(SKColors.White);
+
+            var paint = new SKPaint
+            {
+                IsAntialias = true,
+                Color = new SKColor(0, 0, 0),
+                TextSize = 20.0f,
+                Style = SKPaintStyle.Fill
+            };
+
+            var yCoord = 0;
+            var nameXCoord = 0;
+            var priceXCoord = 300;
+            var leftoverXCoord = 400;
+
+            canvas.DrawText("Название", nameXCoord + xPadding, yCoord + yPadding, paint);
+            canvas.DrawText("Цена", priceXCoord + xPadding, yCoord + yPadding, paint);
+            canvas.DrawText("В наличии", leftoverXCoord + xPadding, yCoord + yPadding, paint);
+
+
+            foreach (var product in products)
+            {
+                yCoord += 30;
+                var unit = product.IsByWeight ? "кг." : "шт.";
+                canvas.DrawText(product.Name, nameXCoord + xPadding, yCoord + yPadding, paint);
+                canvas.DrawText($"{product.RetailPrice}₽", priceXCoord + xPadding, yCoord + yPadding, paint);
+                canvas.DrawText($"{product.Leftover} {unit}", leftoverXCoord + xPadding, yCoord + yPadding, paint);
+            }
+
+            var image = surface.Snapshot();
+            data = image.Encode(SKEncodedImageFormat.Jpeg, 80);
+        }
+
+        return File(data.ToArray(), "image/jpeg");
     }
 }
