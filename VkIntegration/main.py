d47369c2db47f04aea6ca270ce2951d371c8bde7
Daniil <dadyarri@gmail.com>
2023-01-15 14:55:42 +0300

    feat: implement editing product

--- VkIntegration/main.py
+++ VkIntegration/main.py
@@ -3,6 +3,8 @@ import os
 from fastapi import FastAPI
 from vkbottle import API
 
+from request_bodies.EditProductRequestBody import EditProductRequestBody
+
 app = FastAPI()
 user_vk = API(os.getenv("VK_TOKEN"))
 user_vk.API_VERSION = "5.140"
@@ -13,6 +15,12 @@ async def upload_image():
     return {"message": "Hello World"}
 
 
-@app.get("/updateLeftover")
-async def update_leftover():
-    return {"message": f""}
+@app.post("/editProduct")
+async def edit_product(body: EditProductRequestBody):
+    await user_vk.market.edit(
+        -int(os.getenv("VK_GROUP")),
+        body.marketId,
+        name=body.name,
+        price=body.price,
+        stock_amount=body.leftover,
+    )
---
+++ VkIntegration/request_bodies/EditProductRequestBody.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+
+class EditProductRequestBody(BaseModel):
+    marketId: int
+    name: str | None
+    price: str | None
+    leftover: int | None
