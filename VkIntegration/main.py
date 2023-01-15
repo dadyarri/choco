947400808e3d333b16d86f026d3f6496d58f4f41
Daniil <dadyarri@gmail.com>
2023-01-15 14:40:10 +0300

    feat: init vk api

--- VkIntegration/main.py
+++ VkIntegration/main.py
@@ -1,6 +1,11 @@
+import os
+
 from fastapi import FastAPI
+from vkbottle import API
 
 app = FastAPI()
+user_vk = API(os.getenv("VK_TOKEN"))
+user_vk.API_VERSION = "5.140"
 
 
 @app.get("/uploadImage")
