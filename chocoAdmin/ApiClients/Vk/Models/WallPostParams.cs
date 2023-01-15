28ab15e2406d48ab1b2fbceba0c31edfecbc7bf8
Daniil <dadyarri@gmail.com>
2023-01-15 12:06:26 +0300

    feat: add model to wall post request

--- chocoAdmin/ApiClients/Vk/Models/WallPostParams.cs
+++ chocoAdmin/ApiClients/Vk/Models/WallPostParams.cs
@@ -2,5 +2,9 @@ namespace choco.ApiClients.Vk.Models;
 
 public class WallPostParams
 {
+    public bool FromGroup { get; set; }
+    public string Message { get; set; }
+    public List<string> Attachments { get; set; }
+    public bool CloseComments { get; set; }
     
 }
