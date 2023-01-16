d814e83525a4d7cb027bdf74c934035688bb0885
Daniil <dadyarri@gmail.com>
2023-01-16 10:46:46 +0300

    fix: change uri of vk integration api

--- chocoAdmin/Program.cs
+++ chocoAdmin/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddControllersWithViews(options => { options.UseGeneralRoutePre
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 builder.Services.AddHttpClient<VkServiceClient>(client =>
 {
-    client.BaseAddress = new Uri("vkIntegration");
+    client.BaseAddress = new Uri("http://vkIntegration:5679");
 });
 builder.Services.AddSingleton<VkServiceClient>();
 
