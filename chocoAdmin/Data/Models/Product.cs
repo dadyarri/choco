f1100d96426d56c6d733d74de1b9267544c2d747
Daniil <dadyarri@gmail.com>
2023-01-16 10:51:03 +0300

    db: add marketId to product

--- chocoAdmin/Data/Models/Product.cs
+++ chocoAdmin/Data/Models/Product.cs
@@ -3,37 +3,38 @@ namespace choco.Data.Models;
 /// <summary>
 /// Модель, представляющая товар
 /// </summary>
-public class Product: BaseModel
+public class Product : BaseModel
 {
     /// <summary>
     /// Категория товара (например: молочный, горький...)
     /// </summary>
     public ProductCategory Category { get; set; }
-    
+
     /// <summary>
     /// Название товара
     /// </summary>
     public string Name { get; set; }
-    
+
     /// <summary>
     /// Розничная цена
     /// </summary>
     public int RetailPrice { get; set; }
-    
+
     /// <summary>
     /// Оптовая цена
     /// </summary>
     public int WholesalePrice { get; set; }
-    
+
     /// <summary>
     /// Флаг, указывающий на то, должен ли товар продаваться "на развес"
     /// </summary>
     public bool IsByWeight { get; set; }
-    
+
     /// <summary>
     /// Количество товара, оставшегося на складе
     /// </summary>
     public double Leftover { get; set; }
 
     public bool Deleted { get; set; }
+    public int MarketId { get; set; }
 }
