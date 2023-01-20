cadcb39c9ab2cdc415a65c102f7fca63c7544ad0
Daniil <dadyarri@gmail.com>
2023-01-21 02:40:09 +0300

    feat: change model for vk integration's sake

--- chocoAdmin/ApiClients/VkService/RequestBodies/EditProductRequestBody.cs
+++ chocoAdmin/ApiClients/VkService/RequestBodies/EditProductRequestBody.cs
@@ -1,9 +1,15 @@
+using System.Text.Json.Serialization;
+
 namespace choco.ApiClients.VkService.RequestBodies;
 
 public class EditProductRequestBody
 {
+    [JsonPropertyName("marketId")]
     public int MarketId { get; set; }
-    public string Name { get; set; }
-    public int Price { get; set; }
-    public int Leftover { get; set; }
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+    [JsonPropertyName("price")]
+    public int? Price { get; set; }
+    [JsonPropertyName("leftover")]
+    public int? Leftover { get; set; }
 }
