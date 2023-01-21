e9320f002282d5910fc87a541083cfbbbb5b38c4
Daniil <dadyarri@gmail.com>
2023-01-21 03:59:16 +0300

    fix: add property names

--- chocoAdmin/ApiClients/VkService/RequestBodies/ReplacePostRequestBody.cs
+++ chocoAdmin/ApiClients/VkService/RequestBodies/ReplacePostRequestBody.cs
@@ -1,7 +1,11 @@
+using System.Text.Json.Serialization;
+
 namespace choco.ApiClients.VkService.RequestBodies;
 
 public class ReplacePostRequestBody
 {
+    [JsonPropertyName("photo")]
     public string Photo { get; set; }
+    [JsonPropertyName("text")]
     public string? Text { get; set; }
 }
