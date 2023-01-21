5042103ade0c17f6ae3aaa2bb680994aadff1ef3
Daniil <dadyarri@gmail.com>
2023-01-21 12:01:57 +0300

    feat: add displaying text instead of table when no products left

--- chocoAdmin/Utils/ReplacePostUtil.cs
+++ chocoAdmin/Utils/ReplacePostUtil.cs
@@ -55,64 +55,72 @@ public class ReplacePostUtil
                 Typeface = SKTypeface.FromFamilyName("Roboto")
             };
 
-            var yCoord = 0;
-            var nameXCoord = 20;
-            var priceXCoord = 425;
-            var leftoverXCoord = 525;
-
-            canvas.DrawText("Название", nameXCoord + xPadding + 5, yCoord + yPadding, paint);
-            canvas.DrawText("Цена", priceXCoord + xPadding + 5, yCoord + yPadding, paint);
-            canvas.DrawText("В наличии", leftoverXCoord + xPadding + 5, yCoord + yPadding, paint);
-
-            canvas.DrawLine(
-                new SKPoint(nameXCoord + xPadding - 1, yCoord + yPadding - 20),
-                new SKPoint(leftoverXCoord + xPadding + 106, yCoord + yPadding - 20),
-                paint
-            );
-
-            canvas.DrawLine(
-                new SKPoint(nameXCoord + xPadding, yCoord + yPadding + 10),
-                new SKPoint(leftoverXCoord + xPadding + 105, yCoord + yPadding + 10),
-                paint
-            );
-
-            foreach (var product in products)
+            if (products.Count > 0)
             {
-                yCoord += 30;
-                var unit = product.IsByWeight ? "кг." : "шт.";
+                var yCoord = 0;
+                var nameXCoord = 20;
+                var priceXCoord = 425;
+                var leftoverXCoord = 525;
+
+                canvas.DrawText("Название", nameXCoord + xPadding + 5, yCoord + yPadding, paint);
+                canvas.DrawText("Цена", priceXCoord + xPadding + 5, yCoord + yPadding, paint);
+                canvas.DrawText("В наличии", leftoverXCoord + xPadding + 5, yCoord + yPadding, paint);
+
                 canvas.DrawLine(
+                    new SKPoint(nameXCoord + xPadding - 1, yCoord + yPadding - 20),
+                    new SKPoint(leftoverXCoord + xPadding + 106, yCoord + yPadding - 20),
+                    paint
+                );
+
+                canvas.DrawLine(
+                    new SKPoint(nameXCoord + xPadding, yCoord + yPadding + 10),
+                    new SKPoint(leftoverXCoord + xPadding + 105, yCoord + yPadding + 10),
+                    paint
+                );
+
+                foreach (var product in products)
+                {
+                    yCoord += 30;
+                    var unit = product.IsByWeight ? "кг." : "шт.";
+                    canvas.DrawLine(
+                        new SKPoint(nameXCoord + xPadding, yCoord + yPadding + 5),
+                        new SKPoint(leftoverXCoord + xPadding + 105, yCoord + yPadding + 5),
+                        paint
+                    );
+                    canvas.DrawText(product.Name, nameXCoord + xPadding + 5, yCoord + yPadding, paint);
+                    canvas.DrawText($"{product.RetailPrice}₽", priceXCoord + xPadding + 5, yCoord + yPadding, paint);
+                    canvas.DrawText($"{product.Leftover} {unit}", leftoverXCoord + xPadding + 5, yCoord + yPadding,
+                        paint);
+                }
+
+                canvas.DrawLine(
+                    new SKPoint(nameXCoord + xPadding, 0 + yPadding - 20),
                     new SKPoint(nameXCoord + xPadding, yCoord + yPadding + 5),
+                    paint
+                );
+
+                canvas.DrawLine(
+                    new SKPoint(priceXCoord + xPadding, 0 + yPadding - 20),
+                    new SKPoint(priceXCoord + xPadding, yCoord + yPadding + 5),
+                    paint
+                );
+
+                canvas.DrawLine(
+                    new SKPoint(leftoverXCoord + xPadding, 0 + yPadding - 20),
+                    new SKPoint(leftoverXCoord + xPadding, yCoord + yPadding + 5),
+                    paint
+                );
+
+                canvas.DrawLine(
+                    new SKPoint(leftoverXCoord + xPadding + 105, 0 + yPadding - 20),
                     new SKPoint(leftoverXCoord + xPadding + 105, yCoord + yPadding + 5),
                     paint
                 );
-                canvas.DrawText(product.Name, nameXCoord + xPadding + 5, yCoord + yPadding, paint);
-                canvas.DrawText($"{product.RetailPrice}₽", priceXCoord + xPadding + 5, yCoord + yPadding, paint);
-                canvas.DrawText($"{product.Leftover} {unit}", leftoverXCoord + xPadding + 5, yCoord + yPadding, paint);
             }
-
-            canvas.DrawLine(
-                new SKPoint(nameXCoord + xPadding, 0 + yPadding - 20),
-                new SKPoint(nameXCoord + xPadding, yCoord + yPadding + 5),
-                paint
-            );
-
-            canvas.DrawLine(
-                new SKPoint(priceXCoord + xPadding, 0 + yPadding - 20),
-                new SKPoint(priceXCoord + xPadding, yCoord + yPadding + 5),
-                paint
-            );
-
-            canvas.DrawLine(
-                new SKPoint(leftoverXCoord + xPadding, 0 + yPadding - 20),
-                new SKPoint(leftoverXCoord + xPadding, yCoord + yPadding + 5),
-                paint
-            );
-
-            canvas.DrawLine(
-                new SKPoint(leftoverXCoord + xPadding + 105, 0 + yPadding - 20),
-                new SKPoint(leftoverXCoord + xPadding + 105, yCoord + yPadding + 5),
-                paint
-            );
+            else
+            {
+                canvas.DrawText("Товара нет в наличии", xPadding, yPadding, paint);
+            }
 
             var image = surface.Snapshot();
             data = image.Encode(SKEncodedImageFormat.Jpeg, 100);
