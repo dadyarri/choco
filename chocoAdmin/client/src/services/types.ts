0a69c0a08a48918626720cc4accfe87fab20c670
Daniil <dadyarri@gmail.com>
2023-01-25 23:32:48 +0300

    feat: add types for product

---
+++ chocoAdmin/client/src/services/types.ts
@@ -0,0 +1,11 @@
+type Product = {
+    id: string
+    deleted: boolean
+    leftover: number
+    name: string
+    retailPrice: number
+    wholesalePrice: number
+    isByWeight: boolean
+}
+
+export default Product;
