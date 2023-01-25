04020fb5b00b2527fceb4b41956895dbf463fe08
Daniil <dadyarri@gmail.com>
2023-01-25 23:33:12 +0300

    feat: add rendering of table with products

--- chocoAdmin/client/src/pages/warehouse-page.tsx
+++ chocoAdmin/client/src/pages/warehouse-page.tsx
@@ -3,6 +3,10 @@ import {useQuery} from "react-query";
 import HttpService from "../services/http";
 import {toast} from "react-toastify";
 import {BeatLoader} from "react-spinners";
+import {Table} from "react-bootstrap";
+import Product from "../services/types";
+import {GiWeight} from "react-icons/gi";
+import {ImWarning} from "react-icons/im";
 
 const Warehouse: FC = () => {
 
@@ -14,13 +18,35 @@ const Warehouse: FC = () => {
             })
     }
 
-    const {isLoading, isError, data, error} = useQuery('products', fetchProductsList)
+    let {isLoading, isError, data, error} = useQuery<Product[]>('products', fetchProductsList)
 
     return (
         isLoading ?
             <BeatLoader color="#36d7b7"/> :
             <div>
                 <h1>Склад</h1>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Название</th>
+                            <th>Цена</th>
+                            <th>Остаток</th>
+                            <th>Действия</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {data?.map(product =>
+                        (!product.deleted && <tr key={product.id}  className={product.leftover < 0 ?
+                            "table-danger" : ""}>
+                            <td>{product.name} {product.isByWeight ? <GiWeight/> : null}</td>
+                            <td>{product.wholesalePrice} ({product.retailPrice}) &#8381;</td>
+                            <td>{product.leftover} {product.isByWeight ? 'кг.' : 'шт.'} {product.leftover < 0 ?
+                                <ImWarning title={"Количество товара опустилось ниже нуля"}/> : null}</td>
+                            <td></td>
+
+                        </tr>))}
+                    </tbody>
+                </Table>
             </div>
     )
 }
