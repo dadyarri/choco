beb0430359ca0c64b7a259ee4beaab519f60bfe4
Daniil <dadyarri@gmail.com>
2023-01-25 21:54:30 +0300

    fix: update type of route object

--- chocoAdmin/client/src/routes/index.specs.ts
+++ chocoAdmin/client/src/routes/index.specs.ts
@@ -1,8 +1,9 @@
 import {ReactNode} from "react";
 
 type Route = {
-    index: boolean;
-    element: ReactNode
+    index?: boolean;
+    element: ReactNode;
+    path?: string
 }
 
 export default Route;
