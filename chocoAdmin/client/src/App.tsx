930601e977a1adcb5e5759661d469104e23a6cc2
Daniil <dadyarri@gmail.com>
2023-01-25 22:35:36 +0300

    feat: add support of react-query

--- chocoAdmin/client/src/App.tsx
+++ chocoAdmin/client/src/App.tsx
@@ -1,18 +1,39 @@
 import React, {FC} from 'react';
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import Layout from "./components/layout";
 import routes from "./routes";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools";
+import {ToastContainer} from "react-toastify";
 
 const App: FC = () => {
+
+    const queryClient = new QueryClient();
+
     return (
-        <Layout>
-            <Routes>
-                {routes.map((route, index) => {
-                    const { element, ...rest } = route;
-                    return <Route key={index} {...rest} element={element} />;
-                })}
-            </Routes>
-        </Layout>
+        <QueryClientProvider client={queryClient}>
+            <Layout>
+                <Routes>
+                    {routes.map((route, index) => {
+                        const {element, ...rest} = route;
+                        return <Route key={index} {...rest} element={element}/>;
+                    })}
+                </Routes>
+            </Layout>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     );
 }
 
