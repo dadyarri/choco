4d83c2fafe58e90a8b4d83afc91fa1a097f58345
Daniil <dadyarri@gmail.com>
2023-01-25 21:50:17 +0300

    feat: add support of react-router-dom

--- chocoAdmin/client/src/App.tsx
+++ chocoAdmin/client/src/App.tsx
@@ -1,11 +1,19 @@
 import React, {FC} from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Layout from "./components/layout";
+import routes from "./routes";
 
 const App: FC = () => {
-  return (
-    <div>
-
-    </div>
-  );
+    return (
+        <Layout>
+            <Routes>
+                {routes.map((route, index) => {
+                    const { element, ...rest } = route;
+                    return <Route key={index} {...rest} element={element} />;
+                })}
+            </Routes>
+        </Layout>
+    );
 }
 
 export default App;
