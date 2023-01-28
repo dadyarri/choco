10bfc4f3107fffd05fcf3d2c7c879bc0449850fa
Daniil <dadyarri@gmail.com>
2023-01-29 00:24:53 +0300

    feat: add conditional fetching product's info & basic markup

--- chocoAdmin/client/src/pages/product-edit/product-edit.tsx
+++ chocoAdmin/client/src/pages/product-edit/product-edit.tsx
@@ -1,8 +1,37 @@
 import {useParams} from "react-router-dom";
+import {useQuery} from "react-query";
+import Product from "../../services/types";
+import {AxiosError} from "axios";
+import getProductById from "./product-edit.utils";
+import {BeatLoader} from "react-spinners";
+import React from "react";
 
 const ProductEdit = () => {
-    const {productId} = useParams();
-    return <h1>{productId}</h1>
+    let {productId} = useParams();
+
+    const {
+        isLoading,
+        isError,
+        data,
+        error
+    } = useQuery<Product, AxiosError>(
+        ["product", productId],
+        () => getProductById(productId!),
+        {enabled: productId !== undefined}
+    )
+
+    return (
+        isLoading ?
+            <BeatLoader color="#36d7b7"/> :
+            isError ?
+                <div>
+                    <h1>Ошибка загрузки</h1>
+                    <p>{error?.message}</p>
+                </div> :
+                <div>
+                    <h1>{data ? "Редактирование" : "Создание"} товара</h1>
+                </div>
+    )
 }
 
 export default ProductEdit;
