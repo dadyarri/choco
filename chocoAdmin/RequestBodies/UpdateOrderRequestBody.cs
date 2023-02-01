b9ffbaa05753a0bf4377b04b9d7b6ebe67ee7b1b
Daniil <dadyarri@gmail.com>
2023-02-01 16:15:39 +0300

    feat: allow change more fields in order

--- chocoAdmin/RequestBodies/UpdateOrderRequestBody.cs
+++ chocoAdmin/RequestBodies/UpdateOrderRequestBody.cs
@@ -4,8 +4,9 @@ namespace choco.RequestBodies;
 
 public class UpdateOrderRequestBody
 {
+    public DateOnly Date { get; set; }
     public Guid Status { get; set; }
     
-    public Guid OrderId { get; set; }
+    public UpdateOrderAddressRequestBody Address { get; set; }
     
 }
