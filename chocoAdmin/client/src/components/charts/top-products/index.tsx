40c4dc57eb382c6fa10d2789db98ae1fed91a189
Daniil <dadyarri@gmail.com>
2023-02-11 03:32:32 +0300

    feat: add inclining word "заказ" depends on amount of orders

--- chocoAdmin/client/src/components/charts/top-products/index.tsx
+++ chocoAdmin/client/src/components/charts/top-products/index.tsx
@@ -2,15 +2,32 @@ import {ListItem, UnorderedList} from "@chakra-ui/react";
 import {FC} from "react";
 
 type TopProduct = {
-  name: string,
-  value: number
+    name: string,
+    value: number
 };
 type TopProductsProps = {
-  data: TopProduct[]
+    data: TopProduct[]
 }
 
-export const TopProducts: FC<TopProductsProps> = ({ data }) => {
-  return <UnorderedList>
-      {data.map((item) => <ListItem key={item.name}>{item.name} (в {item.value} заказах)</ListItem>)}
-  </UnorderedList>
+export const TopProducts: FC<TopProductsProps> = ({data}) => {
+
+    const inclineWord = (amount: number): string => {
+        const lastDigit = amount % 10;
+        if ((amount % 100 >= 11) && (amount % 100 <= 14)) {
+            return "заказов";
+        } else if (amount === 1) {
+            return "заказе";
+        } else if (lastDigit === 1) {
+            return "заказ";
+        } else if (((lastDigit >= 2) && (lastDigit <= 4)) || ((amount >= 2) && (amount <= 4))) {
+            return "заказах";
+        } else {
+            return "заказов";
+        }
+    };
+
+    return <UnorderedList>
+        {data.map((item) => <ListItem
+            key={item.name}>{item.name} (в {item.value} {inclineWord(item.value)})</ListItem>)}
+    </UnorderedList>
 }
