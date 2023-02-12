9db24c4db15833393af35aedede68f5780573bc9
Daniil <dadyarri@gmail.com>
2023-02-12 12:59:38 +0300

    fix: wrong methods

--- chocoAdmin/client/src/pages/shipment-edit/index.utils.ts
+++ chocoAdmin/client/src/pages/shipment-edit/index.utils.ts
@@ -1,6 +1,7 @@
 import {Shipment, ShipmentStatus} from "../../services/types";
 import HttpService from "../../services/http";
 import {toast} from "react-toastify";
+import {UpdateShipmentRequestBody} from "../../services/request-bodies";
 
 export const getShipmentById = async (orderId: string): Promise<Shipment> => {
     return await HttpService.getShipmentById(orderId)
@@ -24,24 +25,24 @@ export const fetchShipmentStatusesList = async (): Promise<ShipmentStatus[]> => 
 }
 
 
-export const updateShipment = async (orderId: string, values: UpdateShipmentRequestBody): Promise<ShipmentStatus> => {
-    return await HttpService.updateOrder(orderId, values)
+export const updateShipment = async (shipmentId: string, values: UpdateShipmentRequestBody): Promise<ShipmentStatus> => {
+    return await HttpService.updateShipment(shipmentId, values)
         .then((response) => {
-            toast("Заказ обновлён!")
+            toast("Поставка обновлена!")
             return response.data
         })
         .catch((error) => {
-            toast(`Ошибка обновления заказа!
+            toast(`Ошибка обновления поставки!
 ${error?.response.data}`);
             return error;
         })
 }
 
 export const createShipment = async (values: UpdateShipmentRequestBody): Promise<Shipment> => {
-    return await HttpService.createOrder(values)
+    return await HttpService.createShipment(values)
         .then((response) => response.data)
         .catch((error) => {
-            toast(`Ошибка создания заказа!
+            toast(`Ошибка создания поставки!
             ${error?.response.data}`);
             return error;
         })
