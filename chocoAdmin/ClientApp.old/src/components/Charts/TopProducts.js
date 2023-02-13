17000eb8a099a97831d30a9c88c938a34bafa802
Daniil <dadyarri@gmail.com>
2023-02-13 09:46:22 +0300

    refactor: delete old client

--- chocoAdmin/ClientApp.old/src/components/Charts/TopProducts.js
+++
@@ -1,7 +0,0 @@
-import {List} from "reactstrap";
-
-export const TopProducts = ({ data }) => {
-  return <List>
-      {data.map((item) => <li key={item.name}>{item.name} (в {item.value} заказах)</li>)}
-  </List>
-}
