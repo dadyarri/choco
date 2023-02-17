46b83fac37c24da74792abb3073fc6afcaeac7d5
Daniil <dadyarri@gmail.com>
2023-02-17 12:54:24 +0300

    db: add avatar and name to user model

--- chocoAdmin/RequestBodies/RegisterRequestBody.cs
+++ chocoAdmin/RequestBodies/RegisterRequestBody.cs
@@ -1,7 +1,9 @@
 namespace choco.RequestBodies;
 
-public class UserRequestBody
+public class RegisterRequestBody
 {
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
+    public Uri AvatarUri { get; set; }
+    public string Name { get; set; }
 }
