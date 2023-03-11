6e21c402b4ef2269be820eec48fa9463908bf370
Daniil <dadyarri@gmail.com>
2023-03-12 01:06:27 +0300

    fix: add json fields renaming rules

--- chocoAdmin/ApiClients/TelegramService/RequestBodies/SendMessagesRequestBody.cs
+++ chocoAdmin/ApiClients/TelegramService/RequestBodies/SendMessagesRequestBody.cs
@@ -1,7 +1,11 @@
+using System.Text.Json.Serialization;
+
 namespace choco.ApiClients.TelegramService.RequestBodies;
 
 public class SendMessagesRequestBody
 {
+    [JsonPropertyName("message")]
     public string Message { get; set; }
+    [JsonPropertyName("user_ids")]
     public List<long> UserIds { get; set; }
 }
