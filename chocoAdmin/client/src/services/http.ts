455166543a1484f796253b0d2931515fd9f6c7f0
Daniil <dadyarri@gmail.com>
2023-03-11 22:37:14 +0300

    feat: introduce login system with refresh token on client side

--- chocoAdmin/client/src/services/http.ts
+++ chocoAdmin/client/src/services/http.ts
@@ -6,22 +6,30 @@ import {
     UpdateProductRequestBody,
     UpdateShipmentRequestBody
 } from "./request-bodies";
-import {getToken, login, verifyToken} from "./jwt";
+import {getToken, loginByRefreshToken, verifyToken} from "./jwt";
 
 class HttpService {
+
+    private static async refreshToken() {
+        const username = localStorage.getItem("username");
+        const refreshToken = localStorage.getItem("refreshToken");
+
+        if (refreshToken === undefined) {
+            localStorage.removeItem("token");
+            window.location.href = "/";
+        } else if (username && refreshToken) {
+            await loginByRefreshToken(username, refreshToken);
+        } else {
+            window.location.href = "/";
+        }
+    }
+
     private static async getHeaders(): Promise<Headers> {
         const token = getToken();
         if (token) {
             const isValid = await verifyToken(token);
             if (!isValid) {
-                const username = localStorage.getItem("username");
-                const password = localStorage.getItem("password");
-
-                if (username && password) {
-                    await login(username, password)
-                } else {
-                    window.location.href = "/";
-                }
+                await this.refreshToken();
             }
             return {Authorization: `Bearer ${token}`};
         }
--- chocoAdmin/client/src/services/jwt.ts
+++ chocoAdmin/client/src/services/jwt.ts
@@ -1,18 +1,40 @@
-import axios, {AxiosError} from "axios";
+import axios, {AxiosError, AxiosResponse} from "axios";
 import {toast} from "react-toastify";
 
 export const getToken = () => {
     return localStorage.getItem("token");
 }
 
-export const login = (username: string, password: string) => {
-    return axios.post("/api/auth/login", {username, password})
+const updateLocalStorage = (username: string, loginResponse: AxiosResponse) => {
+    localStorage.setItem("username", username);
+    localStorage.setItem("refreshToken", loginResponse.data.refreshToken);
+    localStorage.setItem("token", loginResponse.data.token);
+    localStorage.setItem("name", loginResponse.data.name);
+    localStorage.setItem("avatarUri", loginResponse.data.avatarUri);
+}
+
+export const loginByRefreshToken = (username: string, refreshToken: string) => {
+    return axios.post("/api/auth/passwordLessLogin", {username, refreshToken})
         .then((response) => {
-            localStorage.setItem("username", username);
-            localStorage.setItem("password", password);
-            localStorage.setItem("token", response.data.token);
-            localStorage.setItem("name", response.data.name);
-            localStorage.setItem("avatarUri", response.data.avatarUri);
+            updateLocalStorage(username, response);
+            window.location.reload();
+            return true;
+        })
+        .catch((error: AxiosError) => {
+            if (error.response?.status === 404) {
+                toast("Ошибка входа: Нет такого пользователя");
+            }
+            if (error.response?.status === 403) {
+                toast(`Ошибка входа: Неверный пароль`);
+            }
+            return false;
+        });
+}
+
+export const loginByPassword = (username: string, password: string) => {
+    return axios.post("/api/auth/passwordLogin", {username, password})
+        .then((response) => {
+            updateLocalStorage(username, response);
             window.location.reload();
             return true;
         })
