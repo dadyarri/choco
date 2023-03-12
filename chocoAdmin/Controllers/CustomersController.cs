66607b6d362192884519dcae25c4d68bdb24dfe1
Daniil <dadyarri@gmail.com>
2023-03-12 15:38:40 +0300

    feat: add controller for customers

---
+++ chocoAdmin/Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+using choco.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace choco.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CustomersController: ControllerBase
+{
+    private readonly AppDbContext _db;
+
+    public CustomersController(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<ActionResult> GetAll()
+    {
+        return Ok(await _db.Customers.ToListAsync());
+    }
+
+
+}
