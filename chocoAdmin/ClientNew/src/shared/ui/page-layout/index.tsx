06dc53c9bfc8e959c12daa73694bff0e6048a28c
Daniil <dadyarri@gmail.com>
2023-04-05 02:57:39 +0300

    design: refactor page layout

--- chocoAdmin/ClientNew/src/shared/ui/page-layout/index.tsx
+++ chocoAdmin/ClientNew/src/shared/ui/page-layout/index.tsx
@@ -1,36 +1,50 @@
 import {Layout, Menu, Typography} from "antd";
-import React, {FC, ReactNode} from "react";
+import type {MenuProps} from "antd/es/menu";
+import React, {FC, ReactNode, useState} from "react";
+import {Link} from "react-router-dom";
+import {Route} from "../../../entities/route";
+import routes from "../../config/routes";
 
-const {Header, Content, Footer} = Layout;
+const {Header, Sider, Content, Footer} = Layout;
 const {Title} = Typography;
+type MenuItem = Required<MenuProps>["items"][number];
 
 export const PageLayout: FC<AppLayoutProps> = ({children}) => {
 
+  const [collapsed, setCollapsed] = useState(true);
+
+  const getItem = (
+    {label, path, key, icon}: Route
+  ): MenuItem => {
+    return {
+      key,
+      icon,
+      label: <Link to={path}>{label}</Link>,
+    } as MenuItem;
+  };
+
+
   return <Layout>
-    <Header style={{
-      position: "sticky",
-      top: 0,
-      zIndex: 1,
-      width: "100%",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "space-between"
-    }}>
-      <Title style={{color: "#ffffff"}} level={2}>Шокоадминка</Title>
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={["2"]}
-        items={new Array(3).fill(null).map((_, index) => ({
-          key: String(index + 1),
-          label: `nav ${index + 1}`,
-        }))}
-      />
-    </Header>
-    <Content className="site-layout" style={{padding: "0 10px"}}>
-      {children}
-    </Content>
-    <Footer style={{textAlign: "center"}}>ChocoManager v1.5.0 &copy; 2023 dadyarri</Footer>
+    <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+      <div style={{ height: 32, margin: 16 }} />
+      <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline" items={routes.map((route) => getItem(route))} />
+    </Sider>
+    <Layout>
+      <Header style={{
+        position: "sticky",
+        top: 0,
+        zIndex: 1,
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+      }}>
+        <Title style={{color: "#ffffff"}} level={2}>Шокоадминка</Title>
+      </Header>
+      <Content className="site-layout" style={{padding: "0 10px", minHeight: "84vh"}}>
+        {children}
+      </Content>
+      <Footer style={{textAlign: "center"}}>ChocoManager v1.5.0 &copy; 2023 dadyarri</Footer>
+    </Layout>
   </Layout>;
 };
 
