71fe6410749a572058ce2dc050616c48617c4a39
Daniil <dadyarri@gmail.com>
2023-04-05 02:00:14 +0300

    feat: add empty home page

---
+++ chocoAdmin/ClientNew/src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import {Typography} from "antd";
+import React from "react";
+import {PageLayout} from "../../shared/ui/page-layout";
+
+const {Title} = Typography;
+
+export const HomePage = () => {
+  return <PageLayout>
+    <Title>Статистика</Title>
+  </PageLayout>;
+};
