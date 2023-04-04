e721a37794b753296471d7603d362289a7bdc198
Daniil <dadyarri@gmail.com>
2023-04-05 00:57:50 +0300

    fix: linter errors

--- chocoAdmin/ClientNew/vite.config.ts
+++ chocoAdmin/ClientNew/vite.config.ts
@@ -1,7 +1,7 @@
-import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react-swc";
-import {VitePWA} from "vite-plugin-pwa";
 import eslintPlugin from "@nabla/vite-plugin-eslint";
+import react from "@vitejs/plugin-react-swc";
+import {defineConfig} from "vite";
+import {VitePWA} from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
