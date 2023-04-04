57a48c5b347bae58677f89c997cb3a871b244228
Daniil <dadyarri@gmail.com>
2023-04-05 02:00:50 +0300

    config: configure react-query and react-router

--- chocoAdmin/ClientNew/src/app/index.tsx
+++ chocoAdmin/ClientNew/src/app/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools";
+import {Route, Routes} from "react-router-dom";
+import routes from "../shared/config/routes";
 import {withProviders} from "./providers";
 
 function App() {
 
+  const queryClient = new QueryClient();
+
   return (
-    <div>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        {routes.map((route, index) => {
+          const {element, ...rest} = route;
+          return <Route key={index} {...rest} element={element}/>;
+        })}
+      </Routes>
+      <ReactQueryDevtools initialIsOpen={false}/>
+    </QueryClientProvider>
   );
 }
 
