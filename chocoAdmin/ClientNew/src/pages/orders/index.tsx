63c09b087e67f65667d2e6de7f1ef0b3f39c6b8c
Daniil <dadyarri@gmail.com>
2023-04-05 02:56:03 +0300

    feat: add empty pages

---
+++ chocoAdmin/ClientNew/src/pages/orders/index.tsx
@@ -0,0 +1,11 @@
+import {Typography} from "antd";
+import React from "react";
+import {PageLayout} from "../../shared/ui/page-layout";
+
+const {Title} = Typography;
+
+export const OrdersPage = () => {
+  return <PageLayout>
+    <Title>Заказы</Title>
+  </PageLayout>;
+};
--- chocoAdmin/ClientNew/src/shared/config/routes/index.tsx
+++ chocoAdmin/ClientNew/src/shared/config/routes/index.tsx
@@ -1,12 +1,32 @@
+import {BarChartOutlined, CloudDownloadOutlined, CloudUploadOutlined} from "@ant-design/icons";
 import React from "react";
 import {Route} from "../../../entities/route";
 import {HomePage} from "../../../pages/home";
+import {OrdersPage} from "../../../pages/orders";
+import {ShipmentsPage} from "../../../pages/shipments";
 
 const routes: Route[] = [
   {
     index: true,
     path: "/",
+    key: "home",
     element: <HomePage/>,
+    icon: <BarChartOutlined />,
+    label: "Главная"
+  },
+  {
+    path: "/orders",
+    key: "orders",
+    element: <OrdersPage/>,
+    icon: <CloudDownloadOutlined />,
+    label: "Заказы"
+  },
+  {
+    path: "/shipments",
+    key: "shipments",
+    element: <ShipmentsPage/>,
+    icon: <CloudUploadOutlined />,
+    label: "Поставки"
   }
 ];
 
