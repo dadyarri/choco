f2da686b67923e9206c0221f171a11aadb56841f
Daniil <dadyarri@gmail.com>
2023-04-07 22:40:49 +0300

    refactor: make error handling return both message and error

--- chocoAdmin/client/src/shared/api/lib/index.ts
+++ chocoAdmin/client/src/shared/api/lib/index.ts
@@ -1,23 +1,22 @@
 import axios, {AxiosError} from "axios";
+import {Error} from "entities";
 
-export const handleError = (error: AxiosError | unknown) => {
+export const handleError = (error: AxiosError | unknown): Error => {
+    let message: string;
     if (axios.isAxiosError(error)) {
         if (error.response) {
-            const message: string = `API Error (${error.response.status}): ${error.response.data}`;
+            message = `API Error (${error.response.status}): ${error.response.data}`;
             console.error(message);
-            return message;
         } else if (error.request) {
-            const message: string = 'API Error (No Response)';
+            message = 'API Error (No Response)';
             console.error(message, error.request);
-            return message;
         } else {
-            const message: string = 'API Error';
+            message = 'API Error';
             console.error(message, error.message);
-            return message;
         }
     } else {
-        const message: string = 'Unexpected Error';
+        message = 'Unexpected Error';
         console.error(message, error);
-        return message;
     }
+    return {message: message, error: error};
 }
