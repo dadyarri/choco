9f31b30186e260f10798790fe6739cc5328d27f2
Daniil <dadyarri@gmail.com>
2023-04-07 16:37:21 +0300

    refactor: move routing to separate file

--- chocoAdmin/client/src/app/index.tsx
+++ chocoAdmin/client/src/app/index.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
-import routes from "../routes";
 import 'react-toastify/dist/ReactToastify.min.css';
 import {withProviders} from "./providers";
+import {Routing} from "../pages";
 
 const App = () => {
 
     return (
-        <Routes>
-            {routes.map((route, index) => {
-                const {element, ...rest} = route;
-                return <Route key={index} {...rest} element={element}/>;
-            })}
-        </Routes>
+        <Routing/>
     );
 }
 
---
+++ chocoAdmin/client/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import routes from "../routes";
+
+export const Routing = () => {
+    return (
+        <Routes>
+            {routes.map((route, index) => {
+                const {element, ...rest} = route;
+                return <Route key={index} {...rest} element={element}/>;
+            })}
+        </Routes>
+    );
+};
