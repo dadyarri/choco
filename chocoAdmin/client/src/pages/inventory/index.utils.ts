a55082b465bb7d1238bebf5a0e0f5cb53fafdab8
Daniil <dadyarri@gmail.com>
2023-04-07 22:41:53 +0300

    refactor: use new api client

--- chocoAdmin/client/src/pages/inventory/index.utils.ts
+++ chocoAdmin/client/src/pages/inventory/index.utils.ts
@@ -1,14 +1,18 @@
 import HttpService from "services/http";
 import {toast} from "react-toastify";
 import {InventoryRequestBody} from "services/request-bodies";
+import {products} from "shared/api";
+import {Error} from "entities";
+import {errorHappened, sendToast} from "shared/lib";
 
 export const fetchProductsList = async () => {
-    return await HttpService.getProducts()
-        .then((response) => response.data)
-        .catch((error) => {
-            toast(`Ошибка получения списка товаров!
-            ${error?.response.data}`);
-            throw error;
+    return await products.getAll()
+        .then((data) => {
+            if (errorHappened(data)) {
+                data = data as Error;
+                sendToast(data, "Ошибка получения списка товаров!");
+            }
+            return data;
         })
 }
 
