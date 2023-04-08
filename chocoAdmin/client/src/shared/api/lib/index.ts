294f736c404b814c2f988944c5e2a9223e70758c
Daniil <dadyarri@gmail.com>
2023-04-08 16:45:39 +0300

    refactor: move api client definition to abstract class

--- chocoAdmin/client/src/shared/api/lib/index.ts
+++ chocoAdmin/client/src/shared/api/lib/index.ts
@@ -1,23 +1,74 @@
-import {AxiosError, isAxiosError} from "axios";
+import { AxiosError, isAxiosError } from "axios";
 
-import {Error} from "entities/error";
+import { Error } from "entities/error";
+import api from "shared/config/axios";
 
-export const handleError = (error: AxiosError | unknown): Error => {
-    let message: string;
-    if (isAxiosError(error)) {
-        if (error.response) {
-            message = `API Error (${error.response.status}): ${error.response.data}`;
-            console.error(message);
-        } else if (error.request) {
-            message = "API Error (No Response)";
-            console.error(message, error.request);
-        } else {
-            message = "API Error";
-            console.error(message, error.message);
+export abstract class Api<TModel, TRequestBody> {
+    protected baseURL = "";
+    async getAll(): Promise<TModel[] | Error> {
+        try {
+            const { data } = await api.get<TModel[]>(this.baseURL);
+            return data;
+        } catch (error) {
+            return this.handleError(error);
         }
-    } else {
-        message = "Unexpected Error";
-        console.error(message, error);
     }
-    return {message: message, error: error};
-};
+    async getById(id: string): Promise<TModel | Error> {
+        try {
+            const { data } = await api.get<TModel>(`${this.baseURL}/${id}`);
+            return data;
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+    async create(model: TRequestBody): Promise<TModel | Error> {
+        try {
+            const { data } = await api.post<TModel>(this.baseURL, model);
+            return data;
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+    async update(id: string, model: TRequestBody): Promise<TModel | Error> {
+        try {
+            const { data } = await api.patch<TModel>(`${this.baseURL}/${id}`, model);
+            return data;
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+    async delete(id: string): Promise<void | Error> {
+        try {
+            await api.delete(`${this.baseURL}/${id}`);
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+    async restore(id: string): Promise<void | Error> {
+        try {
+            await api.put(`${this.baseURL}/${id}`);
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+
+    private handleError(error: AxiosError | unknown): Error {
+        let message: string;
+        if (isAxiosError(error)) {
+            if (error.response) {
+                message = `API Error (${error.response.status}): ${error.response.data}`;
+                console.error(message);
+            } else if (error.request) {
+                message = "API Error (No Response)";
+                console.error(message, error.request);
+            } else {
+                message = "API Error";
+                console.error(message, error.message);
+            }
+        } else {
+            message = "Unexpected Error";
+            console.error(message, error);
+        }
+        return { message: message, error: error };
+    }
+}
