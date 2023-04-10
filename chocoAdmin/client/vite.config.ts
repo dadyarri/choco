c0c5c117a4910adb961df9470cfa38b01c2f0b0a
Daniil <dadyarri@gmail.com>
2023-04-11 00:48:34 +0300

    lint: update imports order

--- chocoAdmin/client/vite.config.ts
+++ chocoAdmin/client/vite.config.ts
@@ -1,19 +1,14 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
-import mkcert from 'vite-plugin-mkcert'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
+import svgrPlugin from "vite-plugin-svgr";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
     base: "/app",
     server: {
         https: true,
-        port: 6363
+        port: 6363,
     },
-    plugins: [
-        react(),
-        viteTsconfigPaths(),
-        svgrPlugin(),
-        mkcert()
-    ],
+    plugins: [react(), viteTsconfigPaths(), svgrPlugin(), mkcert()],
 });
