f356fb44972112b46c6545487fde7cee3867e2ae
Daniil <dadyarri@gmail.com>
2023-04-11 02:25:38 +0300

    feat: add some profile page

--- chocoAdmin/client/src/pages/profile/index.tsx
+++ chocoAdmin/client/src/pages/profile/index.tsx
@@ -1,9 +1,23 @@
-import { Switch, Typography } from "@mui/material";
+import { Avatar, Box, Grid, Typography } from "@mui/material";
 import React from "react";
 
 export default () => {
-  return <>
-    <Typography variant={"h2"}>Профиль</Typography>
-    {/*<Switch/>*/}
-  </>;
+    return (
+        <>
+            <Typography variant={"h2"}>Профиль</Typography>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <Grid container sx={{ marginY: "10px" }}>
+                    <Grid item xs={8}>
+                        Привет, Даниил
+                    </Grid>
+                    <Grid item>
+                        <Avatar
+                            alt={"avatar"}
+                            src={"https://www.dadyarri.ru/images/index/webp/avatar.webp"}
+                        ></Avatar>
+                    </Grid>
+                </Grid>
+            </Box>
+        </>
+    );
 };
