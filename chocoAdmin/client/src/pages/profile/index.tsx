c5873b984a70f09fc9ad3940df9c9a61e37a92f6
Daniil <dadyarri@gmail.com>
2023-04-11 02:39:26 +0300

    feat: add some structure to profile page

--- chocoAdmin/client/src/pages/profile/index.tsx
+++ chocoAdmin/client/src/pages/profile/index.tsx
@@ -1,4 +1,15 @@
-import { Avatar, Box, Grid, Typography } from "@mui/material";
+import InfoIcon from "@mui/icons-material/Info";
+import LogoutIcon from "@mui/icons-material/Logout";
+import {
+    Avatar,
+    Box,
+    Divider,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    Typography,
+} from "@mui/material";
 import React from "react";
 
 export default () => {
@@ -6,17 +17,32 @@ export default () => {
         <>
             <Typography variant={"h2"}>Профиль</Typography>
             <Box sx={{ display: "flex", flexDirection: "column" }}>
-                <Grid container sx={{ marginY: "10px" }}>
-                    <Grid item xs={8}>
+                <List>
+                    <ListItem>
+                        <ListItemIcon>
+                            <Avatar
+                                alt={"avatar"}
+                                src={"https://www.dadyarri.ru/images/index/webp/avatar.webp"}
+                            ></Avatar>
+                        </ListItemIcon>
                         Привет, Даниил
-                    </Grid>
-                    <Grid item>
-                        <Avatar
-                            alt={"avatar"}
-                            src={"https://www.dadyarri.ru/images/index/webp/avatar.webp"}
-                        ></Avatar>
-                    </Grid>
-                </Grid>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <LogoutIcon />
+                            </ListItemIcon>
+                            Выйти
+                        </ListItemButton>
+                    </ListItem>
+                    <Divider/>
+                    <ListItem>
+                        <ListItemIcon>
+                            <InfoIcon />
+                        </ListItemIcon>
+                        <i>Версия 1.5.0-dev</i>
+                    </ListItem>
+                </List>
             </Box>
         </>
     );
