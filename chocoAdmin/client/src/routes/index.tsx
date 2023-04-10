52585a5a09a315557287405fa5793d1e1e8833e0
Daniil <dadyarri@gmail.com>
2023-04-11 00:48:45 +0300

    feat: register new routes

--- chocoAdmin/client/src/routes/index.tsx
+++ chocoAdmin/client/src/routes/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-import ProductCategories from "pages/categories";
-import ProductCategoryEdit from "pages/category-edit";
+// import ProductCategories from "pages/categories";
+// import ProductCategoryEdit from "pages/category-edit";
 import Home from "pages/home";
-import { Inventory } from "pages/inventory";
-import { OrderEdit } from "pages/order-edit";
+// import { Inventory } from "pages/inventory";
+// import { OrderEdit } from "pages/order-edit";
 import Orders from "pages/orders";
-import ProductEdit from "pages/product-edit";
-import { ShipmentEdit } from "pages/shipment-edit";
+// import ProductEdit from "pages/product-edit";
+// import { ShipmentEdit } from "pages/shipment-edit";
+import Profile from "pages/profile";
 import Shipments from "pages/shipments";
 import Warehouse from "pages/warehouse";
 
@@ -36,47 +37,52 @@ const routes: Route[] = [
         label: "Склад",
     },
     {
-        path: "/app/inventory",
-        element: <Inventory />,
-        label: "Ревизия",
+        path: "/app/profile",
+        element: <Profile />,
+        label: "Склад",
     },
-    {
-        path: "/app/categories",
-        element: <ProductCategories />,
-        label: "Категории",
-    },
-    {
-        path: "/app/warehouse/add",
-        element: <ProductEdit />,
-    },
-    {
-        path: "/app/warehouse/edit/:productId",
-        element: <ProductEdit />,
-    },
-    {
-        path: "/app/orders/add",
-        element: <OrderEdit />,
-    },
-    {
-        path: "/app/orders/edit/:orderId",
-        element: <OrderEdit />,
-    },
-    {
-        path: "/app/shipments/add",
-        element: <ShipmentEdit />,
-    },
-    {
-        path: "/app/shipments/edit/:shipmentId",
-        element: <ShipmentEdit />,
-    },
-    {
-        path: "/app/categories/add",
-        element: <ProductCategoryEdit />,
-    },
-    {
-        path: "/app/categories/edit/:categoryId",
-        element: <ProductCategoryEdit />,
-    },
+    // {
+    //     path: "/app/inventory",
+    //     element: <Inventory />,
+    //     label: "Ревизия",
+    // },
+    // {
+    //     path: "/app/categories",
+    //     element: <ProductCategories />,
+    //     label: "Категории",
+    // },
+    // {
+    //     path: "/app/warehouse/add",
+    //     element: <ProductEdit />,
+    // },
+    // {
+    //     path: "/app/warehouse/edit/:productId",
+    //     element: <ProductEdit />,
+    // },
+    // {
+    //     path: "/app/orders/add",
+    //     element: <OrderEdit />,
+    // },
+    // {
+    //     path: "/app/orders/edit/:orderId",
+    //     element: <OrderEdit />,
+    // },
+    // {
+    //     path: "/app/shipments/add",
+    //     element: <ShipmentEdit />,
+    // },
+    // {
+    //     path: "/app/shipments/edit/:shipmentId",
+    //     element: <ShipmentEdit />,
+    // },
+    // {
+    //     path: "/app/categories/add",
+    //     element: <ProductCategoryEdit />,
+    // },
+    // {
+    //     path: "/app/categories/edit/:categoryId",
+    //     element: <ProductCategoryEdit />,
+    // },
 ];
 
 export default routes;
