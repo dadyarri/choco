2b48d7a7a74d406de030cb1f714e695bf71c05d7
Daniil <dadyarri@gmail.com>
2023-04-11 15:30:04 +0300

    feat: add auth api

---
+++ chocoAdmin/client/src/shared/api/auth/index.ts
@@ -0,0 +1,47 @@
+import { BaseApi } from "shared/api/lib";
+import api from "shared/config/axios";
+
+export type LoginResponse = {
+    token: string;
+    name: string;
+    avatarUri: string;
+    refreshToken: string;
+};
+
+export class AuthApi extends BaseApi {
+    protected baseURL = "/auth";
+
+    async passwordLogin(username: string, password: string) {
+        try {
+            const { data } = await api.post<LoginResponse>(`${this.baseURL}/passwordLogin`, {
+                username,
+                password,
+            });
+            return data;
+        } catch (error) {
+            throw this.handleError(error);
+        }
+    }
+
+    async passwordLessLogin(username: string, refreshToken: string) {
+        try {
+            const { data } = await api.post<LoginResponse>(`${this.baseURL}/passwordLessLogin`, {
+                username,
+                refreshToken,
+            });
+            return data;
+        } catch (error) {
+            throw this.handleError(error);
+        }
+    }
+
+    async verify() {
+        try {
+            await api.get(`${this.baseURL}/verify`);
+            return true;
+        } catch {
+            return false;
+        }
+    }
+
+}
