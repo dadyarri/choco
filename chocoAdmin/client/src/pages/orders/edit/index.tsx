5722c6fa1e63deb888665c4e089d3ef3fbab88e0
Daniil <dadyarri@gmail.com>
2023-04-13 17:17:58 +0300

    feat: add back button and empty form to edit page

--- chocoAdmin/client/src/pages/orders/edit/index.tsx
+++ chocoAdmin/client/src/pages/orders/edit/index.tsx
@@ -1,8 +1,12 @@
-import { Typography } from "@mui/material";
-import { useEffect } from "react";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import { Button, CircularProgress, Typography } from "@mui/material";
+import { Form, Formik } from "formik";
+import React, { useEffect } from "react";
+import { useQuery } from "react-query";
 import { useParams } from "react-router";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
+import { orderLib } from "entities";
 import { auth } from "features";
 
 const OrderEditPage = () => {
@@ -18,11 +22,31 @@ const OrderEditPage = () => {
 
     const editMode = Boolean(orderId);
 
+    const order = useQuery(["order", orderId], () => orderLib.getOrderById(orderId!), {
+        enabled: editMode,
+    });
+
     return (
         <>
-            <Typography variant={"h4"}>
+            <Typography variant={"h4"} sx={{ marginBottom: 2 }}>
                 {editMode ? "Редактирование" : "Создание"} заказа
             </Typography>
+            <Button
+                sx={{ marginBottom: 2 }}
+                color={"secondary"}
+                variant={"outlined"}
+                component={Link}
+                startIcon={<ChevronLeftIcon />}
+                to={"/app/orders"}
+            >
+                Назад
+            </Button>
+            {order.isLoading && <CircularProgress />}
+            {order.data && (
+                <Formik initialValues={{}} onSubmit={(values) => console.log(values)}>
+                    <Form></Form>
+                </Formik>
+            )}
         </>
     );
 };
