546ab5aa7cb2c66343406b900aa0158998203490
Daniil <dadyarri@gmail.com>
2023-04-13 15:42:38 +0300

    feat: add page with shipments list

--- chocoAdmin/client/src/pages/shipments/index.tsx
+++ chocoAdmin/client/src/pages/shipments/index.tsx
@@ -1,7 +1,42 @@
-import { Typography } from "@mui/material";
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import CheckIcon from "@mui/icons-material/Check";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import PaymentsIcon from "@mui/icons-material/Payments";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import {
+    Box,
+    Button,
+    Chip,
+    CircularProgress,
+    Collapse,
+    Container,
+    IconButton,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    Paper,
+    Stack,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography,
+    useMediaQuery,
+} from "@mui/material";
+import { DateTime } from "luxon";
+import React, { FC, useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { Link, useNavigate } from "react-router-dom";
 
+import { Shipment, ShipmentItem, shipmentLib } from "entities";
 import { auth } from "features";
 
 const ShipmentsPage = () => {
@@ -13,7 +48,186 @@ const ShipmentsPage = () => {
         }
     }, [navigate]);
 
-    return <Typography variant={"h4"}>Поставки</Typography>;
+    const shipments = useQuery("shipments", shipmentLib.getShipments);
+    const isOnDesktop = useMediaQuery("(min-width: 600px)");
+
+    const getStatusChip = (label: string) => {
+        let icon: React.ReactElement | undefined;
+        let color:
+            | "default"
+            | "warning"
+            | "info"
+            | "success"
+            | "primary"
+            | "secondary"
+            | "error"
+            | undefined;
+        switch (label) {
+            case "Обрабатывается": {
+                color = "warning";
+                icon = <WatchLaterIcon />;
+                break;
+            }
+            case "Доставляется": {
+                color = "info";
+                icon = <LocalShippingIcon />;
+                break;
+            }
+            case "Выполнена": {
+                color = "success";
+                icon = <CheckIcon />;
+                break;
+            }
+            case "Отменена": {
+                color = "error";
+                icon = <HighlightOffIcon />;
+                break;
+            }
+            default: {
+                color = "default";
+                icon = undefined;
+            }
+        }
+        return <Chip label={label} color={color} variant={"outlined"} icon={icon} />;
+    };
+
+    const Row: FC<{ shipment: Shipment }> = ({ shipment }) => {
+        const [open, setOpen] = useState(false);
+
+        const total = `${shipment.shipmentItems.reduce(
+            (sum: number, item: ShipmentItem) => sum + item.product.retailPrice * item.amount,
+            0,
+        )} ₽`;
+        return (
+            <>
+                <TableRow key={shipment.id} onClick={() => setOpen(!open)}>
+                    <TableCell>
+                        <IconButton
+                            aria-label="expand row"
+                            size="small"
+                            onClick={() => setOpen(!open)}
+                        >
+                            {open ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+                        </IconButton>
+                    </TableCell>
+                    <TableCell>
+                        {DateTime.fromISO(shipment.date.toLocaleString()).toLocaleString()}
+                    </TableCell>
+                    {isOnDesktop && <TableCell>{getStatusChip(shipment.status.name)}</TableCell>}
+                    <TableCell>
+                        {
+                            <List disablePadding>
+                                {shipment.shipmentItems.map((item: ShipmentItem) => (
+                                    <ListItem key={item.id}>
+                                        {item.product.name} ({item.amount}{" "}
+                                        {item.product.isByWeight ? "кг." : "шт."})
+                                    </ListItem>
+                                ))}
+                            </List>
+                        }
+                    </TableCell>
+                    {isOnDesktop && <TableCell>{total}</TableCell>}
+                </TableRow>
+                <TableRow>
+                    <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                            {!isOnDesktop && (
+                                <Box sx={{ margin: 1 }}>
+                                    <Typography variant="h6" gutterBottom component="div">
+                                        Подробности о поставке
+                                    </Typography>
+                                    <Paper sx={{ maxWidth: 400 }}>
+                                        <List>
+                                            <ListItem>
+                                                <ListItemText>Статус: </ListItemText>
+                                                <ListItemIcon>
+                                                    {getStatusChip(shipment.status.name)}
+                                                </ListItemIcon>
+                                            </ListItem>
+                                            <ListItem>
+                                                <ListItemIcon>
+                                                    <PaymentsIcon />
+                                                </ListItemIcon>
+                                                <ListItemText primary={"Итог"} secondary={total} />
+                                            </ListItem>
+                                        </List>
+                                    </Paper>
+                                </Box>
+                            )}
+                            <Box sx={{ margin: 1 }}>
+                                <Typography variant="h6" gutterBottom component="div">
+                                    Действия
+                                </Typography>
+                                <Paper sx={{ maxWidth: 250, padding: 3 }}>
+                                    <Stack spacing={2}>
+                                        <Button
+                                            color={"success"}
+                                            variant={"outlined"}
+                                            size={"small"}
+                                            startIcon={<VisibilityIcon />}
+                                            component={Link}
+                                            to={`/app/shipments/view/${shipment.id}`}
+                                        >
+                                            Просмотреть
+                                        </Button>
+                                        <Button
+                                            color={"primary"}
+                                            variant={"outlined"}
+                                            size={"small"}
+                                            startIcon={<EditIcon />}
+                                            component={Link}
+                                            to={`/app/shipments/edit/${shipment.id}`}
+                                        >
+                                            Редактировать
+                                        </Button>
+                                        <Button
+                                            color={"error"}
+                                            variant={"outlined"}
+                                            size={"small"}
+                                            startIcon={<DeleteIcon />}
+                                        >
+                                            Удалить
+                                        </Button>
+                                    </Stack>
+                                </Paper>
+                            </Box>
+                        </Collapse>
+                    </TableCell>
+                </TableRow>
+            </>
+        );
+    };
+
+    return (
+        <>
+            <Typography variant={"h4"}>Поставки</Typography>
+            {shipments.isLoading && <CircularProgress />}
+            <Container maxWidth={"xl"}>
+                {shipments.data !== undefined && (
+                    <Paper>
+                        <TableContainer sx={{ overflowX: "initial" }}>
+                            <Table stickyHeader>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell />
+                                        <TableCell>Дата</TableCell>
+                                        {isOnDesktop && <TableCell>Статус</TableCell>}
+                                        <TableCell>Содержимое</TableCell>
+                                        {isOnDesktop && <TableCell>Итог</TableCell>}
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {shipments.data.map((shipment: Shipment) => (
+                                        <Row shipment={shipment} key={shipment.id} />
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Paper>
+                )}
+            </Container>
+        </>
+    );
 };
 
 export default ShipmentsPage;
