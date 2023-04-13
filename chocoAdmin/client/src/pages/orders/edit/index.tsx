71bc938a96c60f71647b2efca5df8717b72ecade
Daniil <dadyarri@gmail.com>
2023-04-13 16:08:55 +0300

    feat: add authentication and variable title

--- chocoAdmin/client/src/pages/orders/edit/index.tsx
+++ chocoAdmin/client/src/pages/orders/edit/index.tsx
@@ -1,7 +1,30 @@
 import { Typography } from "@mui/material";
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import { useNavigate } from "react-router-dom";
+
+import { auth } from "features";
 
 const OrderEditPage = () => {
-    return <Typography variant={"h4"}>Редактирование заказа</Typography>;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!auth.hasToken()) {
+            navigate("/app/login");
+        }
+    }, [navigate]);
+
+    const { orderId } = useParams();
+
+    const editMode = Boolean(orderId);
+
+    return (
+        <>
+            <Typography variant={"h4"}>
+                {editMode ? "Редактирование" : "Создание"} заказа
+            </Typography>
+        </>
+    );
 };
 
 export default OrderEditPage;
